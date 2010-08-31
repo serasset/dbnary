6818987162096f19d476782b92288c6a2cb97ded
serasset <serasset@ilma.imag.fr>
2010-08-31 12:21:18 +0000

    Modified ISO639 tests so that it tests both ISO639_1 and ISO639_3 classes.

--- modules/wiktionary-support/src/test/java/org/getalp/blexisma/wiktionary/ISO639Test.java
+++ modules/wiktionary-support/src/test/java/org/getalp/blexisma/wiktionary/ISO639Test.java
@@ -17,6 +17,15 @@ public class ISO639Test {
     }
     
     @Test
+    public void testOnlyOneLanguage3() {
+        ISO639_3 isoLanguages = ISO639_3.sharedInstance;
+        ISO639_3.Lang fre = isoLanguages.getLang("fre");
+        ISO639_3.Lang fra = isoLanguages.getLang("fra");
+        
+        assertSame("Different Languages", fra, fre);
+    }
+    
+    @Test
     public void testFrenchName() {
         ISO639_1 isoLanguages = ISO639_1.sharedInstance;
         String french = isoLanguages.getLanguageNameInEnglish("fre");
@@ -33,11 +42,28 @@ public class ISO639Test {
     }
 
     @Test
+    public void testEnglishName3() {
+        ISO639_3 isoLanguages = ISO639_3.sharedInstance;
+        String en = isoLanguages.getLanguageNameInEnglish("eng");
+        
+        assertEquals("eng is not English", "English", en);
+    }
+
+    @Test
     public void testFrenchIsFrancais() {
         ISO639_1 isoLanguages = ISO639_1.sharedInstance;
         ISO639_1.Lang french = isoLanguages.getLang("fre");
         
         assertEquals("Français eponyme n'est pas français", french.fr, french.epo);
+    }
+    
+    @Test
+    public void testFrenchIsFrancais3() {
+        ISO639_3 isoLanguages = ISO639_3.sharedInstance;
+        String fepo = isoLanguages.getEponymLanguageName("fre");
+        String ffr = isoLanguages.getLanguageNameInFrench("fre");
+        
+        assertEquals("Français eponyme n'est pas français", ffr, fepo);
     }
     
 }

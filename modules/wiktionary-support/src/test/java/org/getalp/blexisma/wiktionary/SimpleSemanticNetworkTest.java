eeebb3698ea4612768f819b69e78fa1f8dc61abf
serasset <serasset@ilma.imag.fr>
2010-10-07 13:04:34 +0000

    Fixed a buggy test case on Semantic Network's addNode().

--- modules/wiktionary-support/src/test/java/org/getalp/blexisma/wiktionary/SimpleSemanticNetworkTest.java
+++ modules/wiktionary-support/src/test/java/org/getalp/blexisma/wiktionary/SimpleSemanticNetworkTest.java
@@ -33,19 +33,17 @@ public class SimpleSemanticNetworkTest {
         sn1.addRelation(s, s1, 1, "x");
         sn1.addRelation(s, s2, 1, "x");
         
-        assertEquals(sn1.getEdges("Origin").size(), 1);
+        assertEquals(sn1.getNbNodes(), 2);
         
         sn1.addRelation(s, s2, 1, "y");
 
-        assertEquals(sn1.getEdges("Origin").size(), 2);
+        assertEquals(sn1.getEdges("Origin").size(), 3);
 
         Iterator<SimpleSemanticNetwork<String,String>.Relation> it = sn1.getEdges("Origin").iterator();
         String t1 = it.next().getDestination();
         String t2 = it.next().getDestination();
         
         assertEquals(t1, t2);
-
-        assertEquals(sn1.getNbNodes(), 1);
     }
     
 }

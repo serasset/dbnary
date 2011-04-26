cf61e29f199c8865c5da94af77588795363ecbec
alabadie <alabadie@ilma.imag.fr>
2011-04-26 08:06:28 +0000

    Everything necessary for the learning process : - The new SemanticLearning plugin - Update of the society and every plugin to manage the learning process - Save and load option for RAM_VectorialBase - And many minor modification

--- modules/wiktionary-support/src/main/java/org/getalp/blexisma/wiktionary/WiktionaryBasedSemanticDictionary.java
+++ modules/wiktionary-support/src/main/java/org/getalp/blexisma/wiktionary/WiktionaryBasedSemanticDictionary.java
@@ -141,5 +141,8 @@ public class WiktionaryBasedSemanticDictionary implements SemanticDictionary {
 		return vectorialBase;
 	}
 	
-	
+	public void saveDictionary(String pathBase, String pathNetwork)
+	{
+		//TODO procedure de sauvegarde de l'ensemble des données
+	}
 }

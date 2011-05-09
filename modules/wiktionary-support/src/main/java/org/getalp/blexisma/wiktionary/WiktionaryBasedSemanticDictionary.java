7c5b6800293484face7a2dfd4676b6c61902bd5d
serasset <serasset@ilma.imag.fr>
2011-05-09 08:14:11 +0000

    Cleared and extended interface specification for the SemanticDictionary Corrected WiktionaryBasedSemanticDictionary to implement the specification Modified the SemanticAnalysis unit test so that it conforms to the specifications of a Semantic Dictionary Cleared part of the RAM_VectorialBase to make explicit certain implementation choices.

--- modules/wiktionary-support/src/main/java/org/getalp/blexisma/wiktionary/WiktionaryBasedSemanticDictionary.java
+++ modules/wiktionary-support/src/main/java/org/getalp/blexisma/wiktionary/WiktionaryBasedSemanticDictionary.java
@@ -3,7 +3,6 @@ package org.getalp.blexisma.wiktionary;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;

197123d4d8b3efaf7e9f76f4694597f4e359afe7
serasset <serasset@ilma.imag.fr>
2011-06-16 11:47:28 +0000

    Added a command line to generate a Random vectorial base for a lexical network. This is useful to generate real size test bases.

---
+++ modules/wiktionary-support/src/main/java/org/getalp/blexisma/wiktionary/cli/GenerateRandowVectorialBaseForNetwork.java
@@ -0,0 +1,182 @@
+package org.getalp.blexisma.wiktionary.cli;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.getalp.blexisma.impl.vectorialbase.RAM_VectorialBase;
+import org.getalp.blexisma.wiktionary.SimpleSemanticNetwork;
+import org.getalp.blexisma.wiktionary.StringSemNetGraphMLizer;
+import org.getalp.blexisma.wiktionary.TextOnlySemnetReader;
+import org.getalp.blexisma.wiktionary.WiktionaryIndexerException;
+
+public class GenerateRandowVectorialBaseForNetwork {
+
+	private static Options options = null; // Command line options
+
+	private static final String COEFF_VAR_OPTION = "c";
+	private static final double DEFAULT_COEFF_VAR = 3;
+
+	private static final String DIMENSION_OPTION = "d";
+	private static final int DEFAULT_DIMENSION = 2000;
+
+	private static final String ENCODING_SIZE_OPTION = "s";
+	private static final int DEFAULT_ENCODING_SIZE = 32764;
+
+	private static final String INPUT_FORMAT_OPTION = "f";
+	private static final String DEFAULT_INPUT_FORMAT = "raw";
+	
+	private static final String ENCODING_OPTION = "e";
+	private static final String DEFAULT_ENCODING = "UTF-8";
+
+	
+	private CommandLine cmd = null; // Command Line arguments
+	
+	private String inputFormat = DEFAULT_INPUT_FORMAT;
+	private double coeffVar = DEFAULT_COEFF_VAR;
+	private String encoding = DEFAULT_ENCODING;
+	private int dimension, encodingSize;
+	
+	private InputStreamReader input;
+
+	private String output;
+
+	static{
+		options = new Options();
+		options.addOption("h", false, "Prints usage and exits. ");	
+		options.addOption(INPUT_FORMAT_OPTION, true, "Specifies the input format (raw, graphml). " + DEFAULT_INPUT_FORMAT + " by default.");	
+		options.addOption(COEFF_VAR_OPTION, true, "Specifies the variation coefficient used for random vector generation. " + DEFAULT_COEFF_VAR + " by default.");	
+		options.addOption(DIMENSION_OPTION, true, "Specifies the dimension of vectors. " + DEFAULT_DIMENSION + " by default.");	
+		options.addOption(ENCODING_SIZE_OPTION, true, "Specifies the dimension of vectors. " + DEFAULT_ENCODING_SIZE + " by default.");	
+		options.addOption(ENCODING_OPTION, true, 
+				"Encoding of input and output. " + DEFAULT_ENCODING + " by default.");
+	}
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws WiktionaryIndexerException 
+	 */
+	public static void main(String[] args) throws WiktionaryIndexerException, IOException {
+		GenerateRandowVectorialBaseForNetwork cliProg = new GenerateRandowVectorialBaseForNetwork();
+		cliProg.loadArgs(args);
+		cliProg.generate();
+	}
+	
+	private void generate() throws IOException {
+		// TODO: use graphml as an input format for semantic networks.
+		//long start = System.currentTimeMillis();
+		SimpleSemanticNetwork<String,String> sn = new SimpleSemanticNetwork<String,String>(500000,1000000);
+		
+		if ("raw".equals(inputFormat)) {
+			TextOnlySemnetReader.readFromReader(sn, new BufferedReader(input));
+		} else if ("graphml".equals(inputFormat)) {
+			System.err.println("graphml format is currently unsupported as an input format");
+			System.exit(1);
+		} else {
+			System.err.println("Unsupported input format: " + inputFormat);
+			System.exit(1);
+		}
+		
+		RAM_VectorialBase vb = new RAM_VectorialBase(encodingSize, dimension);
+		
+		Iterator<String> it = sn.getNodesIterator();
+		
+		while(it.hasNext()) {
+			String elem = it.next();
+			vb.addVector(elem, vb.nextRandomCV(coeffVar));
+		}
+		
+		vb.save(output);
+	}
+
+	/**
+	 * Validate and set command line arguments.
+	 * Exit after printing usage if anything is astray
+	 * @param args String[] args as featured in public static void main()
+	 */
+	private void loadArgs(String[] args){
+		CommandLineParser parser = new PosixParser();
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.err.println("Error parsing arguments: " + e.getLocalizedMessage());
+			printUsage();
+			System.exit(1);
+		}
+
+		// Check for args
+		if (cmd.hasOption("h")){
+			printUsage();
+			System.exit(0);
+		}
+
+		if (cmd.hasOption(INPUT_FORMAT_OPTION)){
+			inputFormat = cmd.getOptionValue(INPUT_FORMAT_OPTION);
+		}
+
+		if (cmd.hasOption(COEFF_VAR_OPTION)){
+			coeffVar = Double.parseDouble(cmd.getOptionValue(COEFF_VAR_OPTION, Double.toString(DEFAULT_COEFF_VAR)));
+		}
+
+		if (cmd.hasOption(DIMENSION_OPTION)){
+			dimension = Integer.parseInt(cmd.getOptionValue(DIMENSION_OPTION, Integer.toString(DEFAULT_DIMENSION)));
+		}
+
+		if (cmd.hasOption(ENCODING_SIZE_OPTION)){
+			encodingSize = Integer.parseInt(cmd.getOptionValue(ENCODING_SIZE_OPTION, Integer.toString(DEFAULT_ENCODING_SIZE)));
+		}
+
+		if (cmd.hasOption(ENCODING_OPTION)){
+			encoding = cmd.getOptionValue(ENCODING_OPTION);
+		}
+
+		String[] remainingArgs = cmd.getArgs();
+		if (remainingArgs.length != 2) {
+			printUsage();
+			System.exit(1);
+		}
+
+		try {
+
+			String infn = remainingArgs[0];
+			if ("-".equals(infn)) {
+				input = new InputStreamReader(System.in, encoding);
+			} else {
+				input = new InputStreamReader(new FileInputStream(infn), encoding);
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Unsupported encoding.");
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Input File not found.");
+			e.printStackTrace();
+		}
+		
+		output = remainingArgs[1];
+
+	}
+	
+	 public static void printUsage() {
+	    	HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("java -cp /path/to/wiktionary.jar " + GenerateRandowVectorialBaseForNetwork.class.getCanonicalName() + " [OPTIONS] lexical_network_file vectorial_base_file", 
+					"With OPTIONS in:", options, 
+					"specify - as the inputfile to input semnet from STDIN.", false);
+	 }
+}

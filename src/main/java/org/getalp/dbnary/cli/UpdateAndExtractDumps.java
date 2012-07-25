ea50cd577de60ca1098b1d63f950f3061254f834
serasset <serasset@ilma.imag.fr>
2012-07-25 10:31:16 +0000

    Added extraction and encoding of dump file.

--- src/main/java/org/getalp/dbnary/cli/UpdateAndExtractDumps.java
+++ src/main/java/org/getalp/dbnary/cli/UpdateAndExtractDumps.java
@@ -1,8 +1,15 @@
 package org.getalp.dbnary.cli;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.URL;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -13,6 +20,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.getalp.blexisma.api.ISO639_3;
@@ -24,23 +32,23 @@ public class UpdateAndExtractDumps {
 
 	private static final String SERVER_URL_OPTION = "s";
 	private static final String DEFAULT_SERVER_URL = "ftp://ftp.fi.muni.cz/pub/wikimedia/";
-	
+
 	private static final String FORCE_OPTION = "f";
 	private static final boolean DEFAULT_FORCE = false;
-	
+
 	private static final String OUTPUT_DIR_OPTION = "o";
 	private static final String DEFAULT_OUTPUT_DIR = "dumps";
-	
+
 
 
 	private CommandLine cmd = null; // Command Line arguments
-	
+
 	private String outputDir = DEFAULT_OUTPUT_DIR;
 	private boolean force = DEFAULT_FORCE;
 	private String server = DEFAULT_SERVER_URL;
-	
+
 	String[] remainingArgs;
-	
+
 	static{
 		options = new Options();
 		options.addOption("h", false, "Prints usage and exits. ");	
@@ -49,7 +57,7 @@ public class UpdateAndExtractDumps {
 				"force the updating even if a file with the same name already exists in the output directory. " + DEFAULT_FORCE + " by default.");
 		options.addOption(OUTPUT_DIR_OPTION, true, "Output file. " + DEFAULT_OUTPUT_DIR + " by default ");	
 	}
-	
+
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -58,12 +66,62 @@ public class UpdateAndExtractDumps {
 	public static void main(String[] args) throws WiktionaryIndexerException, IOException {
 		UpdateAndExtractDumps cliProg = new UpdateAndExtractDumps();
 		cliProg.loadArgs(args);
-		cliProg.extract();
+		cliProg.updateAndExtract();
 	}
-	
-	private boolean updateDumpFile(String lang) {
+
+
+	private String dumpFileName(String lang, String date) {
+
+		return lang + "wiktionary-"+date+"-pages-meta-history.xml.bz2";
+	}
+
+	/**
+	 * Validate and set command line arguments.
+	 * Exit after printing usage if anything is astray
+	 * @param args String[] args as featured in public static void main()
+	 */
+	private void loadArgs(String[] args){
+		CommandLineParser parser = new PosixParser();
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.err.println("Error parsing arguments: " + e.getLocalizedMessage());
+			printUsage();
+			System.exit(1);
+		}
+
+		// Check for args
+		if (cmd.hasOption("h")){
+			printUsage();
+			System.exit(0);
+		}
+
+		if (cmd.hasOption(SERVER_URL_OPTION)) {
+			server = cmd.getOptionValue(SERVER_URL_OPTION);
+		}
+
+		force = cmd.hasOption(FORCE_OPTION);
+
+		if (cmd.hasOption(OUTPUT_DIR_OPTION)) {
+			outputDir = cmd.getOptionValue(OUTPUT_DIR_OPTION);
+		}
+
+		remainingArgs = cmd.getArgs();
+		if (remainingArgs.length == 0) {
+			printUsage();
+			System.exit(1);
+		}
+
+	}
+
+	public void updateAndExtract() throws WiktionaryIndexerException, IOException {
+		String [] dirs = updateDumpFiles(remainingArgs);
+		uncompressDumpFiles(remainingArgs, dirs);
+	}
+
+	private String updateDumpFile(String lang) {
 		FTPClient client = new FTPClient();
-		 
+
 		try {
 			System.err.println("Updating " + lang);
 			URL url = new URL(server);
@@ -99,8 +157,8 @@ public class UpdateAndExtractDumps {
 				String filename = dumpdir + "/" + dumpFileName(lang,lastDir);
 				File file = new File(filename);
 				if (file.exists() && !force) {
-					System.err.println("Dump file already retreived. Closing...");
-					return true;
+					System.err.println("Dump file " + filename + " already retreived.");
+					return lastDir;
 				}
 				File dumpFile = new File(dumpdir);
 				dumpFile.mkdirs();
@@ -110,80 +168,87 @@ public class UpdateAndExtractDumps {
 				client.retrieveFile(dumpFileName(lang,lastDir),dfile);
 				System.err.println("Retreived " + filename + "[" + (System.currentTimeMillis() - s) + " ms]");
 
+
 			} catch(IOException e){
 				System.out.println(e);
 			}
 			client.logout();
+			return lastDir;
 		} catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            try {
-                client.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return true;
-	}
-	
-	private String dumpFileName(String lang, String date) {
-		
-		return lang + "wiktionary-"+date+"-pages-meta-history.xml.bz2";
+			e.printStackTrace();
+			return null;
+		} finally {
+			try {
+				client.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
-	/**
-	 * Validate and set command line arguments.
-	 * Exit after printing usage if anything is astray
-	 * @param args String[] args as featured in public static void main()
-	 */
-	private void loadArgs(String[] args){
-		CommandLineParser parser = new PosixParser();
+
+	private void uncompressDumpFiles(String[] langs, String[] dirs) {
+		for (int i = 0; i < langs.length; i++) {
+			uncompressDumpFile(langs[i], dirs[i]);
+		}
+	}
+
+	private void uncompressDumpFile(String prefix, String dir) {
+		Reader r = null;
+		Writer w = null;
 		try {
-			cmd = parser.parse(options, args);
-		} catch (ParseException e) {
-			System.err.println("Error parsing arguments: " + e.getLocalizedMessage());
-			printUsage();
-			System.exit(1);
-		}
-		
-		// Check for args
-		if (cmd.hasOption("h")){
-			printUsage();
-			System.exit(0);
-		}
-		
-		if (cmd.hasOption(SERVER_URL_OPTION)) {
-			server = cmd.getOptionValue(SERVER_URL_OPTION);
-		}
-		
-		force = cmd.hasOption(FORCE_OPTION);
-						
-		if (cmd.hasOption(OUTPUT_DIR_OPTION)) {
-			outputDir = cmd.getOptionValue(OUTPUT_DIR_OPTION);
-		}
-				
-		remainingArgs = cmd.getArgs();
-		if (remainingArgs.length == 0) {
-			printUsage();
-			System.exit(1);
-		}
-		
-	}
-	
-    public void extract() throws WiktionaryIndexerException, IOException {
-    	for (String prefix : remainingArgs) {
-    		updateDumpFile(prefix);
-		}
-        
-    }
+			String compressedDumpFile = outputDir + "/" + dir + "/" + dumpFileName(prefix, dir);
+			String uncompressedDumpFile = outputDir + "/" + dir + "/" + prefix + "wkt.xml";
+			
+			System.err.println("uncompressing file :" + compressedDumpFile + " to " + uncompressedDumpFile);
 
-    
-    public static void printUsage() {
-    	HelpFormatter formatter = new HelpFormatter();
+			BZip2CompressorInputStream bzIn = new BZip2CompressorInputStream(new FileInputStream(compressedDumpFile));
+			r = new BufferedReader(new InputStreamReader(bzIn, "UTF-8"));
+
+			FileOutputStream out = new FileOutputStream(uncompressedDumpFile);
+			w = new BufferedWriter(new OutputStreamWriter(out, "UTF-16"));
+
+			final char[] buffer = new char[4096];
+			int len;
+			while ((len = r.read(buffer)) != -1) 
+				w.write(buffer, 0, len); 
+			r.close(); 
+			w.close(); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != r) {
+				try {
+					r.close();
+				} catch (IOException e) {
+					// nop
+				}
+			}
+			if (null != w) {
+				try {
+					w.close();
+				} catch (IOException e) {
+					// nop
+				}
+			}
+		}
+	}
+
+	private String[] updateDumpFiles(String[] langs) {
+		String[] res = new String[langs.length];
+		int i = 0;
+		for (String prefix : remainingArgs) {
+			res[i] = updateDumpFile(prefix);
+			i++;
+		}
+		return res;
+	}
+
+	public static void printUsage() {
+		HelpFormatter formatter = new HelpFormatter();
 		formatter.printHelp("java -cp /path/to/wiktionary.jar org.getalp.dbnary.cli.ExtractWiktionary [OPTIONS] languageCode...", 
 				"With OPTIONS in:", options, 
 				"languageCode is the wiktionary code for a language (usualy a 2 letter code).", false);
-    }
+	}
 
 }

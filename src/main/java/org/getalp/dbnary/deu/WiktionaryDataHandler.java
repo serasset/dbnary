3600289b6af5fe9d514f8c647621fb32a11139f2
suzanje <suzanje@ilma.imag.fr>
2014-06-27 10:47:44 +0000

    extraction of some forms for words

--- src/main/java/org/getalp/dbnary/deu/WiktionaryDataHandler.java
+++ src/main/java/org/getalp/dbnary/deu/WiktionaryDataHandler.java
@@ -1,16 +1,13 @@
 package org.getalp.dbnary.deu;
 
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.getalp.dbnary.DbnaryModel;
 import org.getalp.dbnary.LemonBasedRDFDataHandler;
-import org.getalp.dbnary.WiktionaryIndex;
-import org.getalp.dbnary.WiktionaryIndexerException;
-import org.getalp.dbnary.wiki.ExpandAllWikiModel;
-import org.getalp.dbnary.wiki.WikiTool;
 
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.function.library.substr;
 
 /**
  * 
@@ -28,8 +25,8 @@ public class WiktionaryDataHandler  extends LemonBasedRDFDataHandler{
 	private final static String advCat="Adverb";
 	private final static String worBindingCat="Wortverbindung";
 
-	private final static String germanMorphoBegin = "\\{\\{Deutsch ";
-	private final static String germanMorphoEnd = "\\}\\}";
+	private final static String germanMorphoBegin = "{{Deutsch ";
+	private final static String germanMorphoEnd = "}}";
 	
 	
 	public WiktionaryDataHandler(String lang) {
@@ -39,59 +36,35 @@ public class WiktionaryDataHandler  extends LemonBasedRDFDataHandler{
 	
 	public void addPartOfSpeech(String originalPOS, Resource normalizedPOS, Resource normalizedType){
 		super.addPartOfSpeech(originalPOS,normalizedPOS,normalizedType);
-		
 		if (((normalizedType == wordEntryType) || (normalizedType == lexEntryType))) {
-			System.out.println("cat : "+originalPOS);
 			String lexEntry=currentLexEntry();
 			String page=wi.getTextOfPage(lexEntry);
-			String formes=extractForms(page,normalizedPOS);
-			if (formes!=null) {
-				//printing the forms
-				//System.out.println(formes);
-			}
+			GermanExtractorWikiModel gewm= new GermanExtractorWikiModel(this, wi, new Locale("de"),"/${image}", "/${title}");
+			gewm.addOtherForms(page, originalPOS);
 		}
 	}
+
 	
-	public String extractForms(String s, Resource normalizedPos){
-		String catWord=getPosString(normalizedPos);
-		
-		if (catWord!=null) {
-			Pattern patternBegin= Pattern.compile(catWord);
-			Pattern patternEnd = Pattern.compile(germanMorphoEnd);
-			Matcher m = patternBegin.matcher(s);
-			int start,end;
-			
-			if (m.find()) {
-			start=m.end();
-			m = patternEnd.matcher(s);
-				if (m.find(start)) {
-					end=m.start();
-					return s.substring(start, end);
-				}
-			}
-		}
-		return null;
-	}
 	
-	/**
-	 * 
-	 * @param normalizedPos
-	 * @return a String to find the beginning of the description
-	 */
-	private String getPosString(Resource normalizedPos){
-		String res;
-		if (normalizedPos==verbPOS){	
-			res=germanMorphoBegin+verbCat;
-		} else if(normalizedPos==adjPOS){
-			res=germanMorphoBegin+adjCat;
-		} else if(normalizedPos==nounPOS){
-			res=germanMorphoBegin+nounCat;
-		} else if(normalizedPos==adverbPOS){
-			res=germanMorphoBegin+advCat;
-		} else {
-			res=null;
-		}
-		return res;
-	}
+	
+//	/**
+//	 * 
+//	 * @param normalizedPos
+//	 * @return a String to find the beginning of the description
+//	 */
+//	private String getPosString(Resource normalizedPos){
+//		String res=germanMorphoBegin;
+//		if (normalizedPos==verbPOS){
+//			res=germanMorphoBegin+verbCat;
+//		} else if(normalizedPos==adjPOS){
+//			res=germanMorphoBegin+adjCat;
+//		} else if(normalizedPos==nounPOS){
+//			res=germanMorphoBegin+nounCat;
+//		} else if(normalizedPos==adverbPOS){
+//			res=germanMorphoBegin+advCat;
+//		}
+//		return res;
+//	}
+
 	
 }

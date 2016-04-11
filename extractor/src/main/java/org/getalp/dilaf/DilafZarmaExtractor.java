bd90d88a6250c677a8411ba6edf943023f81f672
serasset <serasset@ilma.imag.fr>
2016-04-11 09:43:00 +0000

    Added DILAF Bambara extraction.

--- extractor/src/main/java/org/getalp/dilaf/DilafZarmaExtractor.java
+++ extractor/src/main/java/org/getalp/dilaf/DilafZarmaExtractor.java
@@ -10,69 +10,13 @@ import org.codehaus.stax2.XMLStreamReader2;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 
-public class DilafZarmaExtractor {
+public class DilafZarmaExtractor extends DilafExtractor {
 
-	private XMLInputFactory2 xmlif;
-	
-	public DilafZarmaExtractor() {
-	     try {
-	            xmlif = (XMLInputFactory2) XMLInputFactory2.newInstance();
-	            xmlif.setProperty(XMLInputFactory.IS_VALIDATING, Boolean.FALSE);
-	            xmlif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.FALSE);
-	            xmlif.setProperty(XMLInputFactory2.P_PRESERVE_LOCATION, Boolean.TRUE);
-	        } catch (Exception ex) {
-	            System.err.println("Cannot intialize XMLInputFactory while creating DilafZarmaExtractor.");
-	            throw new RuntimeException("Cannot initialize XMLInputFactory", ex);
-	        }
+	public DilafZarmaExtractor(DilafLemonDataHandler wdh) {
+		super(wdh);
 	}
-	
-	public void importDilafZarmaXmlFile(File dilafFile, DilafLemonDataHandler wdh) throws DilafExtractorException {
 
-	        // create new XMLStreamReader
-
-	        long starttime = System.currentTimeMillis();
-	        int nbArticles = 0;
-
-	        XMLStreamReader2 xmlr = null;
-	        try {
-	            // pass the file name. all relative entity references will be
-	            // resolved against this as base URI.
-	            xmlr = xmlif.createXMLStreamReader(dilafFile);
-
-	            // check if there are more events in the input stream
-	            while (xmlr.hasNext()) {
-	                xmlr.next();
-	                if (xmlr.isStartElement() && xmlr.getLocalName().equals("dilaf")) {
-	                    // create lemon lexicon instance and link all entries to it.
-	                	// Get source, target, creation date and version number ?
-	                    
-	                } else if (xmlr.isStartElement() && xmlr.getLocalName().equals("article")) {
-	                    importArticle(xmlr, wdh);
-	                    nbArticles++;
-	                }
-	            }
-	        } catch (XMLStreamException ex) {
-	            System.out.println(ex.getMessage());
-
-	            if (ex.getNestedException() != null) {
-	                ex.getNestedException().printStackTrace();
-	            }
-	        } catch (Exception ex) {
-	            ex.printStackTrace();
-	        } finally {
-	            try {
-	                if (xmlr != null)
-	                    xmlr.close();
-	            } catch (XMLStreamException ex) {
-	                ex.printStackTrace();
-	            }
-	        }
-
-	        long endtime = System.currentTimeMillis();
-	        System.out.println(" Parsing Time = " + (endtime - starttime) + "; " + nbArticles + " pages parsed.");
-	    }
-
-	private void importArticle(XMLStreamReader2 xmlr, DilafLemonDataHandler wdh) throws XMLStreamException {
+	protected void importArticle(XMLStreamReader2 xmlr) throws XMLStreamException {
 		String lemma = null, senseNumber = null, pronounciation = null, partOfSpeech = null;
 		Resource lexicalSense = null;
 		while (xmlr.hasNext()) {
@@ -91,7 +35,7 @@ public class DilafZarmaExtractor {
             	wdh.registerTranslations(lexicalSense, translations);
             } else if (xmlr.isStartElement() && xmlr.getLocalName().equals("feeriji")) { // Definition
             	String def = xmlr.getElementText();
-            	wdh.registerDefinition(lexicalSense, def);
+            	wdh.registerDefinition(lexicalSense, def, "dje");
             } else if (xmlr.isEndElement() && xmlr.getLocalName().equals("article")) {
             	return;
             }
@@ -99,11 +43,16 @@ public class DilafZarmaExtractor {
     	
     	
 	}
-	
-	public static void main(String args[]) throws DilafExtractorException {
-		DilafZarmaExtractor e = new DilafZarmaExtractor();
-		DilafLemonDataHandler wdh = new DilafLemonDataHandler("dje");
-		e.importDilafZarmaXmlFile(new File(args[0]), wdh);
-		wdh.dump(System.out, "TURTLE");
+
+	@Override
+	protected String rootElement() {
+		return "dilaf";
 	}
+
+	@Override
+	protected String articleElement() {
+		return "article";
+	}
+
+
 }

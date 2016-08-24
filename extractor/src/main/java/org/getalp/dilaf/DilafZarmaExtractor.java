316cb84037565f9f7ba613e5f8204e06653ced2c
serasset <Gilles.Serasset@imag.fr>
2016-08-24 18:24:32 +0200

    Code Reformating in DILAF extractor

--- extractor/src/main/java/org/getalp/dilaf/DilafZarmaExtractor.java
+++ extractor/src/main/java/org/getalp/dilaf/DilafZarmaExtractor.java
@@ -1,58 +1,53 @@
 package org.getalp.dilaf;
 
-import java.io.File;
-
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-
-import org.codehaus.stax2.XMLInputFactory2;
+import com.hp.hpl.jena.rdf.model.Resource;
 import org.codehaus.stax2.XMLStreamReader2;
 
-import com.hp.hpl.jena.rdf.model.Resource;
+import javax.xml.stream.XMLStreamException;
 
 public class DilafZarmaExtractor extends DilafExtractor {
 
-	public DilafZarmaExtractor(DilafLemonDataHandler wdh) {
-		super(wdh);
-	}
+    public DilafZarmaExtractor(DilafLemonDataHandler wdh) {
+        super(wdh);
+    }
 
-	protected void importArticle(XMLStreamReader2 xmlr) throws XMLStreamException {
-		String lemma = null, senseNumber = null, pronounciation = null, partOfSpeech = null;
-		Resource lexicalSense = null;
-		while (xmlr.hasNext()) {
+    protected void importArticle(XMLStreamReader2 xmlr) throws XMLStreamException {
+        String lemma = null, senseNumber = null, pronounciation = null, partOfSpeech = null;
+        Resource lexicalSense = null;
+        while (xmlr.hasNext()) {
             xmlr.next();
             if (xmlr.isStartElement() && xmlr.getLocalName().equals("sanniize")) { // entrée
-            	int sni = xmlr.getAttributeIndex(null, "lamba");
-            	senseNumber = (sni == -1) ? "1" : xmlr.getAttributeValue(sni);
-            	lemma = xmlr.getElementText();
+                int sni = xmlr.getAttributeIndex(null, "lamba");
+                senseNumber = (sni == -1) ? "1" : xmlr.getAttributeValue(sni);
+                lemma = xmlr.getElementText();
             } else if (xmlr.isStartElement() && xmlr.getLocalName().equals("ciiyaŋ")) { // transcription phonétique
-            	pronounciation = xmlr.getElementText();
+                pronounciation = xmlr.getElementText();
             } else if (xmlr.isStartElement() && xmlr.getLocalName().equals("kanandi")) { // part of speech
-            	partOfSpeech = xmlr.getElementText();
-            	lexicalSense = wdh.registerNewLexicalSense(lemma, partOfSpeech, pronounciation, senseNumber);
+                partOfSpeech = xmlr.getElementText();
+                lexicalSense = wdh.registerNewLexicalSense(lemma, partOfSpeech, pronounciation, senseNumber);
             } else if (xmlr.isStartElement() && xmlr.getLocalName().equals("bareyaŋ")) { // French translation
-            	String translations = xmlr.getElementText();
-            	wdh.registerTranslations(lexicalSense, translations);
+                String translations = xmlr.getElementText();
+                wdh.registerTranslations(lexicalSense, translations);
             } else if (xmlr.isStartElement() && xmlr.getLocalName().equals("feeriji")) { // Definition
-            	String def = xmlr.getElementText();
-            	wdh.registerDefinition(lexicalSense, def, "dje");
+                String def = xmlr.getElementText();
+                wdh.registerDefinition(lexicalSense, def, "dje");
             } else if (xmlr.isEndElement() && xmlr.getLocalName().equals("article")) {
-            	return;
+                return;
             }
-		}
-    	
-    	
-	}
+        }
 
-	@Override
-	protected String rootElement() {
-		return "dilaf";
-	}
 
-	@Override
-	protected String articleElement() {
-		return "article";
-	}
+    }
+
+    @Override
+    protected String rootElement() {
+        return "dilaf";
+    }
+
+    @Override
+    protected String articleElement() {
+        return "article";
+    }
 
 
 }

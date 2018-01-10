4397b357db347b210fc0c2f63161f75c1746adb5
hannes <hannes.pirker@oeaw.ac.at>
2018-01-10 18:21:16 +0100

    GermanSubstantiveDeklination: minor adjustments for debugging

--- dbnary-extractor/src/main/java/org/getalp/dbnary/deu/GermanSubstantiveDeklinationExtractorWikiModel.java
+++ dbnary-extractor/src/main/java/org/getalp/dbnary/deu/GermanSubstantiveDeklinationExtractorWikiModel.java
@@ -9,6 +9,7 @@ import org.getalp.dbnary.IWiktionaryDataHandler;
 import org.getalp.dbnary.WiktionaryIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.collection.concurrent.Debug;
 
 
 public class GermanSubstantiveDeklinationExtractorWikiModel extends GermanTableExtractorWikiModel {
@@ -25,15 +26,25 @@ public class GermanSubstantiveDeklinationExtractorWikiModel extends GermanTableE
   protected GermanInflectionData getInflectionDataFromCellContext(List<String> context) {
     GermanInflectionData inflection = new GermanInflectionData();
     boolean isArticleColumn = false;
+    boolean hasGender = false;
+    String numberIndex = "";
     for (String h : context) {
       h = h.trim();
       switch (h) {
         case "Singular":
+          inflection.number = GNumber.SINGULAR;
+          break;
         case "Singular 1":
         case "Singular 2":
         case "Singular 3":
         case "Singular 4":
+          // TODO: for Morphisto we would need to "bundle" the Plurals according to their index
+          // (1-4)
           inflection.number = GNumber.SINGULAR;
+          numberIndex = getNumberIndex(h);
+          if (!numberIndex.isEmpty()) {
+            inflection.note.add("number:" + h);
+          }
           break;
         case "Singular m":
           inflection.number = GNumber.SINGULAR;
@@ -44,20 +55,33 @@ public class GermanSubstantiveDeklinationExtractorWikiModel extends GermanTableE
           inflection.genre = Genre.FEMININ;
           break;
         case "Plural":
+          inflection.number = GNumber.PLURAL;
+          break;
+
         case "Plural 1":
         case "Plural 2":
         case "Plural 3":
         case "Plural 4":
+          // TODO: for Morphisto we would need to "bundle" the Plurals according to their index
+          // (1-4)
           inflection.number = GNumber.PLURAL;
+          numberIndex = getNumberIndex(h);
+          log.debug("h={} numberIndex={}", h, numberIndex);
+          if (!numberIndex.isEmpty()) {
+            inflection.note.add("number:" + h);
+          }
           break;
         case "Maskulinum":
           inflection.genre = Genre.MASCULIN;
+          hasGender = true;
           break;
         case "Femininum":
           inflection.genre = Genre.FEMININ;
+          hasGender = true;
           break;
         case "Neutrum":
           inflection.genre = Genre.NEUTRUM;
+          hasGender = true;
           break;
         case "Artikel":
           isArticleColumn = true;
@@ -89,7 +113,18 @@ public class GermanSubstantiveDeklinationExtractorWikiModel extends GermanTableE
     if (isArticleColumn) {
       return null;
     }
+    // check whether Substantive does have a gender - warn otherwise
+    // TODO: not quite sure what to do. One possibility is to guess Gender from the article
+    if (!hasGender) {
+      log.debug("Warning: no gender in Substantive entry.");
+    }
     return inflection;
+  }
+
+  // brutal: get number by removing all non-numbers
+  // "Plural 1" => "1"
+  public String getNumberIndex(String str) {
+    return str.replaceAll("[^0-9]+", "");
   }
 
 }

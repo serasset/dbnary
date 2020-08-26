7ee4bb6cf2d01c5d8a37c5577323a98c9f52a7a4
Gilles Sérasset <gilles.serasset@imag.fr>
2020-08-26 09:53:46 +0200

    Corrected script to avoid code style issues.

--- kaiko/misc/fix_all_stats.sh
+++ kaiko/misc/fix_all_stats.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-SEDSCRIPT=~/develop/dbnary/kaiko/
-pushd $HOME/develop/wiktionary/extracts/ontolex
+SEDSCRIPT=~/develop/dbnary/kaiko/misc/fix_stats.sed
+pushd "$HOME/develop/wiktionary/extracts/ontolex"
 mkdir ~/stats.saved
-cp ??/??_dbnary_stat* ~/stats.saved
-for lg in ?? do
-	pushd $lg
-	# iterate over all dates...
-	for f in ${lg}_dbanry_stat*.ttl.bz2 do
-		bzcat $f | sed -f ${SEDSCRIPT} | bzip2 > $f.new
-	done
-	popd
+cp "./??/??_dbnary_stat*" ~/stats.saved
+for lg in ??; do
+  echo ${lg}
+  pushd "${lg}"
+  # iterate over all dates...
+  for f in ${lg}_dbnary_stat*.ttl.bz2; do
+    echo $f
+    bzcat "$f" | sed -f "${SEDSCRIPT}" | bzip2 > "$f.new"
+  done
+  popd
 done
 

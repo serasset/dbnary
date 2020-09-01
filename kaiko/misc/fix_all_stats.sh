3c84b3087a8d3d26e5743132706ddb644b3137b3
Gilles Sérasset <gilles.serasset@imag.fr>
2020-09-01 17:21:47 +0200

    Improved shell script to process and correct all statistics.

--- kaiko/misc/fix_all_stats.sh
+++ kaiko/misc/fix_all_stats.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-SEDSCRIPT=~/develop/dbnary/kaiko/
-pushd $HOME/develop/wiktionary/extracts/ontolex
-mkdir ~/stats.saved
-cp ??/??_dbnary_stat* ~/stats.saved
+SEDSCRIPT="~/develop/dbnary/kaiko/fix_stats.sed"
+SAVEDIR="$HOME/stats.saved/`date +%F_%H-%M-%S`/"
+DATADIR="$HOME/develop/wiktionary/extracts/ontolex"
+mkdir -p $SAVEDIR
+rsync -a $DATADIR/ $SAVEDIR/
+
+pushd $DATADIR
 for lg in ?? do
 	pushd $lg
 	# iterate over all dates...
 	for f in ${lg}_dbanry_stat*.ttl.bz2 do
 		bzcat $f | sed -f ${SEDSCRIPT} | bzip2 > $f.new
+		mv $f $f.old
+		mv $f.new $f
 	done
 	popd
 done

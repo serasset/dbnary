240e78af1c25411c713206612dd6ce5b65cdf688
Gilles Sérasset <gilles.serasset@imag.fr>
2021-07-09 10:18:26 +0200

    Added usage information to dbnary.sh script

--- kaiko/extractor/dbnary.sh
+++ kaiko/extractor/dbnary.sh
@@ -16,7 +16,36 @@ TDB="--tdb"
 DATE=""
 VERBOSE=""
 
-while getopts ":d:t:v:D:nmMeElLsSTVxX" opt; do
+help() {
+  echo "USAGE: $0 [OPTIONS] lg1 lg2..."
+  echo "Update and extract wiktionary dumps with DBnary."
+  echo "  By default, DBnary will fetch the latest available version on wikimedia dump mirror if it "
+  echo "  is not already available on the local host."
+  echo "where:"
+  echo "  lg1 lg2... are language codes (usually 2 letter codes as used in wiktionary language editions)"
+  echo "  with OPTIONS in:"
+  echo "    -h                : display this help message"
+  echo "    -T                : use TDB to store extracted data (necessary for big wiktionaries)"
+  echo "    -V                : enable verbose mode"
+  echo "    -h                : display this help message"
+  echo "    -d  <package>     : enable DEBUG log level for the specified java package"
+  echo "    -t  <package>     : enable TRACE log level for the specified java package"
+  echo "    -v  <version>     : use the specified version of DBnary extractor"
+  echo "    -D  <version>     : use the specified version of wiktionary dumps (older dumps may not be available online)"
+  echo "    -n                : do not fetch anything from the network but extract the latest dumps available on the local host"
+  echo "    -m                : enable extraction of morphological data (enabled by default)"
+  echo "    -M                : disable extraction of morphological data"
+  echo "    -l                : enable extraction of LIME metadata (enabled by default)"
+  echo "    -L                : disable extraction of LIME metadata"
+  echo "    -s                : enable extraction of statistics (enabled by default)"
+  echo "    -S                : disable extraction of statistics"
+  echo "    -e                : enable extraction of etymology (enabled by default)"
+  echo "    -E                : disable extraction of etymology"
+  echo "    -x                : enable computation of data enhancement (e.g. translation source disambiguation, enabled by default)"
+  echo "    -X                : disable computation of data enhancement"
+}
+
+while getopts ":d:t:v:D:nmMeElLsSTVxXh" opt; do
   case $opt in
     d)
       DEBUG="${DEBUG} -Dorg.slf4j.simpleLogger.log.${OPTARG}=debug"
@@ -68,6 +97,10 @@ while getopts ":d:t:v:D:nmMeElLsSTVxX" opt; do
       ;;
     V)
       VERBOSE="-v"
+      ;;
+    h)
+      help
+      exit 0
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2

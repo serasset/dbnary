697383f0b7c554d308812968736de74a40a31087
Gilles Sérasset <gilles.serasset@imag.fr>
2021-09-01 12:06:38 +0200

    Deprecating POS states that appeared outside of Model based data structures (wiktionaryPOS and lexinfoPOS)

--- dbnary-extractor/src/main/java/org/getalp/model/ontolex/LexicalEntry.java
+++ dbnary-extractor/src/main/java/org/getalp/model/ontolex/LexicalEntry.java
@@ -1,5 +1,6 @@
 package org.getalp.model.ontolex;
 
+import java.util.HashSet;
 import java.util.Set;
 import org.apache.jena.rdf.model.Resource;
 import org.getalp.dbnary.DBnaryOnt;
@@ -8,19 +9,58 @@ import org.getalp.dbnary.rdfutils.URI;
 
 public class LexicalEntry {
   private String name;
-  private String partOfSpeech;
-  private int entryNumber;
+  private String wiktionaryPartOfSpeech;
+  private int entryNumber = 0;
 
-  private Set<Resource> types;
-  private Set<Resource> partOfSpeeches;
+  private Set<Resource> types = new HashSet<>();
+  private Set<Resource> partOfSpeeches = new HashSet<>();
 
   LexicalForm canonicalForm;
   Set<LexicalForm> otherForms;
 
   public LexicalEntry() {}
 
-  public LexicalEntry(LexicalForm canonicalForm) {
-    this.canonicalForm = canonicalForm;
+  public LexicalEntry(String name) {
+    this.name = name;
+  }
+
+  public LexicalEntry(String name, String pos) {
+    this(name);
+    this.wiktionaryPartOfSpeech = pos;
+  }
+
+  public LexicalEntry(String name, String pos, int entryNumber) {
+    this(name, pos);
+    this.entryNumber = entryNumber;
+  }
+
+  public int getEntryNumber() {
+    return entryNumber;
+  }
+
+  public void setEntryNumber(int entryNumber) {
+    this.entryNumber = entryNumber;
+  }
+
+  public String getWiktionaryPartOfSpeech() {
+    return wiktionaryPartOfSpeech;
+  }
+
+  public Resource getLexinfoPartOfSpeech() {
+    return partOfSpeeches.stream().findFirst().orElse(null);
+  }
+
+  public void setWiktionaryPartOfSpeech(String wiktionaryPartOfSpeech) {
+    this.wiktionaryPartOfSpeech = wiktionaryPartOfSpeech;
+  }
+
+  public void setPartOfSpeech(Resource pos) {
+    this.partOfSpeeches.clear();
+    this.partOfSpeeches.add(pos);
+  }
+
+  public void addResourceType(Resource type) {
+    this.types.add(type);
   }
 
   public Resource attachTo(Resource page) {
@@ -35,7 +75,7 @@ public class LexicalEntry {
   private String computeResourceName(Resource page) {
     String lexEntryPrefix = URI.getNameSpace(page);
 
-    return lexEntryPrefix + canonicalForm + "__" + partOfSpeech + "__" + entryNumber;
+    return lexEntryPrefix + name + "__" + wiktionaryPartOfSpeech + "__" + entryNumber;
   }
 
 

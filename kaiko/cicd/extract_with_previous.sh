28b2a675ad673dc5c9ec74d1e7cc8f5684d877ea
Gilles Sérasset <gilles.serasset@imag.fr>
2021-09-23 17:16:57 +0200

    Fixed: the extraction process should not compress results.

--- kaiko/cicd/extract_with_previous.sh
+++ kaiko/cicd/extract_with_previous.sh
@@ -6,4 +6,4 @@ echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source ${SCRIPT_DIR}/settings
 
 # Extract data using PR version
-DBNARY_DIR=/tmp/$NEXT_VERSION/ ${SCRIPT_DIR}/../extractor/dbnary.sh -V -v $NEXT_VERSION -c $SAMPLE_SIZE $LANGS
+DBNARY_DIR=/tmp/$NEXT_VERSION/ ${SCRIPT_DIR}/../extractor/dbnary.sh -V -Z -v $NEXT_VERSION -c $SAMPLE_SIZE $LANGS

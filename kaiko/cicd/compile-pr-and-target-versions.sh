fbf8e6c92095b4ce1b06c5cab674fa8c731a8e1b
Gilles Sérasset <gilles.serasset@imag.fr>
2021-09-23 15:01:56 +0200

    Separated scripts in order to have a clearer pipeline.

---
+++ kaiko/cicd/compile-pr-and-target-versions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
+source ${SCRIPT_DIR}/settings
+
+# Compile PR and DESTINATION versions
+if [ x$BITBUCKET_PR_DESTINATION_BRANCH != x ]; then
+    mvn versions:set -B -DnewVersion=$NEXT_VERSION
+    mvn install
+    git checkout $BITBUCKET_PR_DESTINATION_BRANCH
+    mvn versions:set -B -DnewVersion=$PREVIOUS_VERSION
+    mvn install
+else
+    echo "No Pull Request destination branch, is this really a Pull Request ?"
+    exit 1
+fi
+
+# Prepare directory layout
+mkdir -p /tmp/$NEXT_VERSION-extracts/
+mkdir -p /tmp/$PREVIOUS_VERSION-extracts/
+mkdir -p $DIFFS

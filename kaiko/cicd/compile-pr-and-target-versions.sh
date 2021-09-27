62f10f156b52268f6b0f93178cafc72b1922d01b
Gilles Sérasset <gilles.serasset@imag.fr>
2021-09-27 21:01:47 +0200

    Fixed incorrect versions compiled due to incorrect checkout of the target branch

--- kaiko/cicd/compile-pr-and-target-versions.sh
+++ kaiko/cicd/compile-pr-and-target-versions.sh
@@ -4,11 +4,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source ${SCRIPT_DIR}/settings
+set -x
 
 # Compile PR and DESTINATION versions
 if [ x$BITBUCKET_PR_DESTINATION_BRANCH != x ]; then
     mvn versions:set -B -DnewVersion=$NEXT_VERSION
     mvn install
+    git stash -u
     git checkout $BITBUCKET_PR_DESTINATION_BRANCH
     mvn versions:set -B -DnewVersion=$PREVIOUS_VERSION
     mvn install

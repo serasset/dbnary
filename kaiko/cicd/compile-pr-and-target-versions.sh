becb642eaeb101ede79694af96a24a93fe3c8d58
Gilles Sérasset <gilles.serasset@imag.fr>
2021-09-27 21:26:43 +0200

    Update cicd commands from develop branch

--- kaiko/cicd/compile-pr-and-target-versions.sh
+++ kaiko/cicd/compile-pr-and-target-versions.sh
@@ -4,23 +4,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source ${SCRIPT_DIR}/settings
-
-# Prepare directory layout
-mkdir -p /tmp/$NEXT_VERSION/
-mkdir -p /tmp/$PREVIOUS_VERSION/
-mkdir -p $DIFFS
-
-# Share the dumps directory between both versions to avoid reloading the dumps for each version
-mkdir -p /tmp/$PREVIOUS_VERSION/dumps
-ln -s ../$PREVIOUS_VERSION/dumps /tmp/$NEXT_VERSION/dumps
-
-
-ls -al /tmp/$NEXT_VERSION /tmp/$PREVIOUS_VERSION
+set -x
 
 # Compile PR and DESTINATION versions
 if [ x$BITBUCKET_PR_DESTINATION_BRANCH != x ]; then
     mvn versions:set -B -DnewVersion=$NEXT_VERSION
     mvn install
+    git stash -u
     git checkout $BITBUCKET_PR_DESTINATION_BRANCH
     mvn versions:set -B -DnewVersion=$PREVIOUS_VERSION
     mvn install
@@ -28,4 +18,3 @@ else
     echo "No Pull Request destination branch, is this really a Pull Request ?"
     exit 1
 fi
-

67523f548e232e44619c56b2997a5fc43c308b16
Gilles Sérasset <gilles.serasset@imag.fr>
2021-11-23 17:46:42 +0100

    CICD validation now summarize difference in etymology, morphology and enhancement along with the usual ontolex data parts

--- kaiko/cicd/compute_differences.sh
+++ kaiko/cicd/compute_differences.sh
@@ -5,9 +5,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source "${SCRIPT_DIR}/settings"
 
-# compute differences
-echo "${SCRIPT_DIR}/../extractor/compute_diffs.sh -v $NEXT_VERSION -f /tmp/$PREVIOUS_VERSION/extracts/ontolex/latest/ -t /tmp/$NEXT_VERSION/extracts/ontolex/latest/ -d $DIFFS $LANGS"
-"${SCRIPT_DIR}/../extractor/compute_diffs.sh" -v "$NEXT_VERSION" -f "/tmp/$PREVIOUS_VERSION/extracts/ontolex/latest/" -t "/tmp/$NEXT_VERSION/extracts/ontolex/latest/" -d "$DIFFS" $LANGS
+for model in ontolex morphology etymology enhancement
+do
+  # compute differences
+  echo "${SCRIPT_DIR}/../extractor/compute_diffs.sh -v $NEXT_VERSION -f /tmp/$PREVIOUS_VERSION/extracts/ontolex/latest/ -t /tmp/$NEXT_VERSION/extracts/ontolex/latest/ -d $DIFFS $LANGS"
+  "${SCRIPT_DIR}/../extractor/compute_diffs.sh" -v "$NEXT_VERSION" -f "/tmp/$PREVIOUS_VERSION/extracts/ontolex/latest/" -t "/tmp/$NEXT_VERSION/extracts/ontolex/latest/" -d "$DIFFS" $LANGS
+done
 
 # Move diffs to the build dir as artifacts are only possible there
 mkdir -p target/

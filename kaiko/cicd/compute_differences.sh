286196042f9f33c71de7db6da93d11a550f5cbfc
Gilles Sérasset <gilles.serasset@imag.fr>
2021-11-24 10:41:24 +0100

    Split CICD validation in 2 phases (extraction + differences) to avoid hitting the 2h limit

--- kaiko/cicd/compute_differences.sh
+++ kaiko/cicd/compute_differences.sh
@@ -5,13 +5,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source "${SCRIPT_DIR}/settings"
 
+mkdir -p $DIFFS
+
 for model in ontolex morphology etymology enhancement
 do
   # compute differences
-  echo "${SCRIPT_DIR}/../extractor/compute_diffs.sh -v $NEXT_VERSION -f /tmp/$PREVIOUS_VERSION/extracts/ontolex/latest/ -t /tmp/$NEXT_VERSION/extracts/ontolex/latest/ -d $DIFFS $LANGS"
-  "${SCRIPT_DIR}/../extractor/compute_diffs.sh" -v "$NEXT_VERSION" -f "/tmp/$PREVIOUS_VERSION/extracts/ontolex/latest/" -t "/tmp/$NEXT_VERSION/extracts/ontolex/latest/" -d "$DIFFS" $LANGS
+  echo "${SCRIPT_DIR}/../extractor/compute_diffs.sh -v $NEXT_VERSION -f target/extracts/$PREVIOUS_VERSION/extracts/ontolex/latest/ -t target/extracts/$NEXT_VERSION/extracts/ontolex/latest/ -d $DIFFS $LANGS"
+  "${SCRIPT_DIR}/../extractor/compute_diffs.sh" -v "$NEXT_VERSION" -f "target/extracts/$PREVIOUS_VERSION/extracts/ontolex/latest/" -t "target/extracts/$NEXT_VERSION/extracts/ontolex/latest/" -d "$DIFFS" $LANGS
 done
-
-# Move diffs to the build dir as artifacts are only possible there
-mkdir -p target/
-mv /tmp/diffs target/diffs

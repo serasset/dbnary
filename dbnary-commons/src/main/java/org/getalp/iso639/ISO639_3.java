9582fd05740bca1737f52a5e88ceb7b5fa02e154
Gilles Sérasset <gilles.serasset@imag.fr>
2022-01-04 17:40:11 +0100

    CheckStyle formatting extended to all modules.

--- dbnary-commons/src/main/java/org/getalp/iso639/ISO639_3.java
+++ dbnary-commons/src/main/java/org/getalp/iso639/ISO639_3.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -14,13 +14,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @author serasset Support class for ISO 639-3 standard for language naming. <p> This class is
- * designed to be used as a singleton. <p> Usage: <code>ISO639_3 isoLanguages =
+ * @author serasset Support class for ISO 639-3 standard for language naming.
+ *         <p>
+ *         This class is designed to be used as a singleton.
+ *         <p>
+ *         Usage: <code>ISO639_3 isoLanguages =
  * ISO639_3.sharedInstance; String french = isoLanguages.getLanguageNameInEnglish("fre");</code>
  */
 public class ISO639_3 {
 
-  public class Lang {
+  public static class Lang {
 
     /**
      * @return the id
@@ -77,27 +80,21 @@ public class ISO639_3 {
 
   private final static String linePatternString =
       "^(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)(?:\t(.*))?$";
-  private final static String epolinePatternString =
-      "^(.*?)\t(.*?)$";
-  private final static String uncommonPatternString =
-      "^(.*?)\t(.*?)\t(.*?)\t(.*?)$";
+  private final static String epolinePatternString = "^(.*?)\t(.*?)$";
   private final static Pattern linePattern = Pattern.compile(linePatternString);
   private final static Pattern epolinePattern = Pattern.compile(epolinePatternString);
-  private final static Pattern uncommonPattern = Pattern.compile(uncommonPatternString);
 
   public static ISO639_3 sharedInstance = new ISO639_3();
-  private Map<String, Lang> langMap = new HashMap<String, Lang>();
-  private Set<Lang> langSet = new HashSet<Lang>();
+  private final Map<String, Lang> langMap = new HashMap<>();
+  private final Set<Lang> langSet = new HashSet<>();
 
   private ISO639_3() {
-    //an element of langMap has key "ita"
-    //and value {id: "ita", part2b: "ita", part2t: "ita", part1: "it", en: "Italian"}
-    //another element of langMap has key "it"
-    //and value {id: "ita", part2b: "ita", part2t: "ita", part1: "it", en: "Italian"}
-    InputStream fis = null;
-    try {
-      fis = this.getClass().getResourceAsStream("iso-639-3.tab");
-      BufferedReader br = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+    // an element of langMap has key "ita"
+    // and value {id: "ita", part2b: "ita", part2t: "ita", part1: "it", en: "Italian"}
+    // another element of langMap has key "it"
+    // and value {id: "ita", part2b: "ita", part2t: "ita", part1: "it", en: "Italian"}
+    try (InputStream fis = this.getClass().getResourceAsStream("iso-639-3.tab");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))) {
 
       Matcher matcher = linePattern.matcher("");
 
@@ -106,8 +103,8 @@ public class ISO639_3 {
         matcher.reset(s);
         if (matcher.find()) {
           Lang l = new Lang();
-          // Id   Part2B  Part2T  Part1   Scope   Language_Type   Ref_Name    Comment
-          // ita  ita     ita     it      I       L               Italian
+          // Id___Part2B_Part2T_Part1 Scope Language_Type Ref_Name Comment
+          // ita__ita____ita____it____I_____L_____________Italian
           l.id = matcher.group(1);
           l.part2b = matcher.group(2);
           l.part2t = matcher.group(3);
@@ -133,27 +130,16 @@ public class ISO639_3 {
       }
 
 
-    } catch (UnsupportedEncodingException e) {
-      // This should really never happen
     } catch (IOException e) {
       // don't know what I should do here, as the data should be bundled with the code.
       e.printStackTrace();
-    } finally {
-      if (fis != null) {
-        try {
-          fis.close();
-        } catch (IOException e) {
-          // nop
-        }
-      }
     }
 
     // Get eponym language names
     // TODO: do it lazily.
-    fis = null;
-    try {
-      fis = this.getClass().getResourceAsStream("ISO639-eponym.tab");
-      BufferedReader br = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+    try (InputStream fis = this.getClass().getResourceAsStream("ISO639-eponym.tab");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))
+    ) {
 
       Matcher matcher = epolinePattern.matcher("");
 
@@ -167,36 +153,19 @@ public class ISO639_3 {
           if (l != null) {
             l.epo = matcher.group(2);
           }
-          // else
-          // System.err.println("Unknown language code: " + matcher.group(1));
         } else {
           System.err.println("Unrecognized line:" + s);
         }
         s = br.readLine();
       }
-    } catch (UnsupportedEncodingException e) {
-
     } catch (IOException e) {
       e.printStackTrace();
-    } finally {
-      if (fis != null) {
-        try {
-          fis.close();
-        } catch (IOException e) {
-
-        }
-      }
     }
     // Get French names
     // TODO: do this lazily
-    fis = null;
-    try {
-      fis = this.getClass().getResourceAsStream("ISO639-fr.tab");
-      if (fis == null) {
-        throw new IOException("ISO639 French data not available");
-      }
-      BufferedReader br = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
-
+    try (InputStream fis = this.getClass().getResourceAsStream("ISO639-fr.tab");
+      BufferedReader br = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))
+    ) {
       Matcher matcher = epolinePattern.matcher("");
 
       String s = br.readLine();
@@ -209,27 +178,15 @@ public class ISO639_3 {
           if (l != null) {
             l.fr = matcher.group(2);
           }
-//                         else 
-//                             System.err.println("Unknown language code: " + matcher.group(1));
         } else {
           System.err.println("Unrecognized line:" + s);
         }
         s = br.readLine();
       }
-    } catch (UnsupportedEncodingException e) {
-
     } catch (IOException e) {
+      System.err.println("ISO639 French data not available");
       e.printStackTrace();
-    } finally {
-      if (fis != null) {
-        try {
-          fis.close();
-        } catch (IOException e) {
-
-        }
-      }
     }
-
   }
 
   public String getLanguageNameInFrench(String langcode) {
@@ -279,10 +236,5 @@ public class ISO639_3 {
   public Set<Lang> knownLanguages() {
     return langSet;
   }
-
-  public static void main(String ars[]) {
-    ;
-  }
-
 
 }

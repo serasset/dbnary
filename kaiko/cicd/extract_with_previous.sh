48e6f5a1c022ef9025964fca4ebd5931193001c6
Gilles Sérasset <gilles.serasset@imag.fr>
2022-01-04 20:15:39 +0100

    Use old scripts when validating in cicd

--- kaiko/cicd/extract_with_previous.sh
+++ kaiko/cicd/extract_with_previous.sh
@@ -5,6 +5,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source "${SCRIPT_DIR}/settings"
 
+# Make sure the scripts are the correct ones
+# Compile PR and DESTINATION versions
+if [ "x$BITBUCKET_PR_DESTINATION_BRANCH" == "x" ]; then
+  if [[ "$BITBUCKET_BRANCH" =~ "^feature/.*$" ]]; then
+    BITBUCKET_PR_DESTINATION_BRANCH=develop
+  else
+    BITBUCKET_PR_DESTINATION_BRANCH=master
+  fi
+  echo "Not a Pull Request, I will compare branch with $BITBUCKET_PR_DESTINATION_BRANCH"
+fi
+
+git checkout "$BITBUCKET_PR_DESTINATION_BRANCH" -- kaiko
+
 # Extract data using PR version
 DBNARY_DIR="/tmp/$PREVIOUS_VERSION/" "${SCRIPT_DIR}/../extractor/dbnary.sh" -V -Z -n -v "$PREVIOUS_VERSION" -c "$SAMPLE_SIZE" $LANGS
 

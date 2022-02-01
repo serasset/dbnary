3a538f737737d3442e0dd85f64f83d4752954bad
Gilles Sérasset <gilles.serasset@imag.fr>
2022-02-01 21:40:58 +0100

    Fixed some faulty template substitution in definition extraction of English. Fixed CICD

--- kaiko/cicd/summarize_on_slack.sh
+++ kaiko/cicd/summarize_on_slack.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source "${SCRIPT_DIR}/settings"
 
-"${SCRIPT_DIR}/../extractor/summarize_diffs.sh" -v "$NEXT_VERSION" -d $DIFFS -s
+"out/dbnary/$PREVIOUS_VERSION/bin/dbnary-diff-summary" -d $DIFFS --slack

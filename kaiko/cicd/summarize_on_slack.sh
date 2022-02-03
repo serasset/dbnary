4e809a82b64638202659478eddf517e0d44e3d16
Gilles Sérasset <gilles.serasset@imag.fr>
2022-02-03 11:43:10 +0100

    Switched to appassembler generated commands to avoid the need to follow current version numbers in scripts

--- kaiko/cicd/summarize_on_slack.sh
+++ kaiko/cicd/summarize_on_slack.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
 source "${SCRIPT_DIR}/settings"
 
-"out/dbnary/$PREVIOUS_VERSION/bin/dbnary-diff-summary" --slack $DIFFS
+"$BINDIR/$PREVIOUS_VERSION/bin/dbnary-diff-summary" --slack $DIFFS

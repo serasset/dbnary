2c165aa14efeb0f4e46669e74e60c857dde93daf
Gilles Sérasset <gilles.serasset@imag.fr>
2022-02-07 13:04:45 +0100

    Added scripts to manually evaluate a new version quality (perform the same chack as CICD), locally.

--- kaiko/extractor/evaluate.sh
+++ kaiko/extractor/evaluate.sh
@@ -21,26 +21,9 @@ BATCH[1]="en"
 BATCH[2]="fr pt it fi ru no la"
 BATCH[3]="de el tr ja es bg pl nl sh sv lt mg id ku"
 
-BINDIR=out/dbnary
-echo " ==== EXTRACTING WITH NEXT VERSION ===== "
-for n in 1 2 3
-do
-  BATCH_LANGS=${BATCH[$n]}
-  echo $BATCH_LANGS
-  EFFECTIVE_LANGS=(`echo $BATCH_LANGS $LANGS | tr ' ' '\n' | sort | uniq -d`)
-  echo "Effectively extracting languages : " $BATCH_LANGS "//" $LANGS "-->" ${EFFECTIVE_LANGS[@]}
-  FEATURES="--endolex=ontolex,morphology,lime,etymology,enhancement,statistics --exolex=ontolex"
-  # Extract data using Target branch version
-  if [[ x$EFFECTIVE_LANGS != x ]]
-  then
-    "$BINDIR/$NEXT_VERSION/bin/dbnary" update --dir "$WIKTIONARY_DIR/$NEXT_VERSION/" -v --no-compress $FEATURES --sample "$SAMPLE_SIZE" ${EFFECTIVE_LANGS[@]} &
-    sleep 60
-  fi
-done
-
-echo " ==== EXTRACTING WITH PREVIOUS VERSION ===== "
-for n in 1 2 3
-do
+extract() {
+  n=$1
+  version=$2
   BATCH_LANGS=${BATCH[$n]}
   echo $BATCH_LANGS
   EFFECTIVE_LANGS=(`echo $BATCH_LANGS $LANGS | tr ' ' '\n' | sort | uniq -d`)
@@ -49,17 +32,32 @@ do
   # Extract data using PR version
   if [[ x$EFFECTIVE_LANGS != x ]]
   then
-    "$BINDIR/$PREVIOUS_VERSION/bin/dbnary" update --dir "$WIKTIONARY_DIR/$PREVIOUS_VERSION/" -v --no-compress $FEATURES --sample "$SAMPLE_SIZE" ${EFFECTIVE_LANGS[@]}&
+    "$BINDIR/$NEXT_VERSION/bin/dbnary" update --dir "$WIKTIONARY_DIR/$version/" -v --no-compress $FEATURES --sample "$SAMPLE_SIZE" ${EFFECTIVE_LANGS[@]} &
     sleep 60
   fi
-done
+}
+
+BINDIR=out/dbnary
+echo " ==== EXTRACTING WITH NEXT VERSION ===== "
+extract 1 $NEXT_VERSION &
+extract 2 $NEXT_VERSION  &
+extract 3 $NEXT_VERSION  &
+wait
+
+echo " ==== EXTRACTING WITH PREVIOUS VERSION ===== "
+extract 1 $PREVIOUS_VERSION &
+extract 2 $PREVIOUS_VERSION  &
+extract 3 $PREVIOUS_VERSION  &
+wait
 mkdir -p $DIFFS
+FROM=$WIKTIONARY_DIR/$PREVIOUS_VERSION/extracts/ontolex/latest
+TO=$WIKTIONARY_DIR/$NEXT_VERSION/extracts/ontolex/latest
 
 for model in ontolex morphology etymology enhancement exolex_ontolex
 do
   # compute differences
-  echo ./kaiko/extractor/compute_diffs.sh -m $model -f "$WIKTIONARY_DIR/$PREVIOUS_VERSION/" -t "$WIKTIONARY_DIR/$NEXT_VERSION/" -d $DIFFS $LANGS
-  PATH=$BINDIR/$NEXT_VERSION/bin:$PATH ./kaiko/extractor/compute_diffs.sh -m $model -f "$WIKTIONARY_DIR/$PREVIOUS_VERSION/" -t "$WIKTIONARY_DIR/$NEXT_VERSION/" -d $DIFFS $LANGS
+  echo ./kaiko/extractor/compute_diffs.sh -m $model -f "$FROM" -t "$TO" -d $DIFFS $LANGS
+  PATH=$BINDIR/$NEXT_VERSION/bin:$PATH ./kaiko/extractor/compute_diffs.sh -m $model -f "$FROM" -t "$TO" -d $DIFFS $LANGS
 done
 
-./kaiko/cicd/compute_differences.sh
+"$BINDIR/$NEXT_VERSION/bin/dbnary-diff-summary" $DIFFS

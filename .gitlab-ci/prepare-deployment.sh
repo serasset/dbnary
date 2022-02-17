8b7c5136e2aafdffca54e9cfa13e0ff00ca4e27a
Gilles Sérasset <gilles.serasset@imag.fr>
2022-02-17 17:20:51 +0100

    Cleaning up the CICD before going back to gitflow

--- .gitlab-ci/prepare-deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SSH_DIR=${HOME}/.ssh
-mkdir -p $SSH_DIR
-chmod 700 $SSH_DIR
-cat $IDENTITY > $SSH_DIR/id_rsa
-chmod 600 $SSH_DIR/id_rsa
-cat $KNOWN_HOSTS >> $SSH_DIR/known_hosts
-chmod 600 $SSH_DIR/known_hosts

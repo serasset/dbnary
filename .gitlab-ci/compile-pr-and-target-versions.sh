a2a22a46d5771d60c1ebda8273ab4cdfeaf34e0f
Gilles Sérasset <gilles.serasset@imag.fr>
2022-02-17 18:40:08 +0100

    Applying contrastive evaluation to gitlab CICD

---
+++ .gitlab-ci/compile-pr-and-target-versions.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
+source "${SCRIPT_DIR}/settings"
+
+echo "Bash Version : ${BASH_VERSION}"
+echo "Commit Message = $CI_COMMIT_MESSAGE"
+echo "Validating on languages : $LANGS"
+echo "Current Branch : $NEXT_VERSION_BRANCH"
+echo "Pull request destination branch : $PREVIOUS_VERSION_BRANCH"
+
+set -x
+
+# make sure folder is in gitignore to avoid losing the packaged app after git stash and mvn clean
+echo out >> .gitignore
+mkdir -p $BINDIR/$NEXT_VERSION
+mvn versions:set -B -DnewVersion="$NEXT_VERSION"
+mvn package
+cp -r dbnary-commands/target/distributions/dbnary/*.tar.gz $BINDIR/$NEXT_VERSION
+mvn clean
+
+git stash -u
+git checkout "$PREVIOUS_VERSION_BRANCH"
+echo out >> .gitignore
+
+mkdir -p $BINDIR/$PREVIOUS_VERSION
+mvn versions:set -B -DnewVersion="$PREVIOUS_VERSION"
+mvn package
+cp -r dbnary-commands/target/distributions/dbnary/*.tar.gz $BINDIR/$PREVIOUS_VERSION
+mvn clean
+
+# Then, switch back to latest branch so that latest improvement in CI/CD are used.
+git stash -u
+git checkout "$NEXT_VERSION"

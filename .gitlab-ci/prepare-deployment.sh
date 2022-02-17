666ab35bfe45bb7868b4e3014f09ae84b74a6187
Gilles Sérasset <gilles.serasset@imag.fr>
2022-02-17 16:38:31 +0100

    Switching back to maven repository on gitlab

--- .gitlab-ci/prepare-deployment.sh
+++ .gitlab-ci/prepare-deployment.sh
@@ -5,7 +5,5 @@ mkdir -p $SSH_DIR
 chmod 700 $SSH_DIR
 cat $IDENTITY > $SSH_DIR/id_rsa
 chmod 600 $SSH_DIR/id_rsa
-cat $SSH_CONFIG >> $SSH_DIR/config
-chmod 644 $SSH_DIR/config
 cat $KNOWN_HOSTS >> $SSH_DIR/known_hosts
 chmod 600 $SSH_DIR/known_hosts

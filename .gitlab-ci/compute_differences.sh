a4360be105297fa1da30c6487c5c82d757013c80
Gilles Sérasset <gilles.serasset@imag.fr>
2022-03-12 19:16:56 +0100

    picking CICD from develop branch

---
+++ .gitlab-ci/compute_differences.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
+source "${SCRIPT_DIR}/settings"
+
+mkdir -p $DIFFS
+
+for model in ontolex morphology etymology enhancement exolex_ontolex
+do
+  # compute differences
+  echo "${CI_PROJECT_DIR}/kaiko/extractor/compute_diffs.sh -m $model -f target/evaluation/$PREVIOUS_VERSION/ -t target/evaluation/$NEXT_VERSION/ -d $DIFFS -x $NEXT_VERSION_BIN/rdfdiff $LANGS"
+  "${CI_PROJECT_DIR}/kaiko/extractor/compute_diffs.sh" -m $model -f "target/evaluation/$PREVIOUS_VERSION/" -t "target/evaluation/$NEXT_VERSION/" -d "$DIFFS" -x "$NEXT_VERSION_BIN/rdfdiff" $LANGS
+done
---
+++ .gitlab-ci/summarize_on_slack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Sourcing settings from: ${SCRIPT_DIR}/settings"
+source "${SCRIPT_DIR}/settings"
+
+"$NEXT_VERSION_BIN/dbnary-diff-summary" --slack $DIFFS

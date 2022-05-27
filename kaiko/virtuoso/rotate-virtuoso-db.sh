808982aa4af4395d31eddbc5dba08b39012258b0
Gilles Sérasset <gilles.serasset@imag.fr>
2022-05-27 10:45:47 +0200

    fix(virtuoso): database rotation script did not build indexes and lookup tables correctly

--- kaiko/virtuoso/rotate-virtuoso-db.sh
+++ kaiko/virtuoso/rotate-virtuoso-db.sh
@@ -398,13 +398,13 @@ echoln "========================================================" ;
 
 -- Build Full Text Indexes by running the following commands using the Virtuoso isql program
 -- With this rule added, all text in all graphs will be indexed...
-echoln --- Setting up indexing
+echoln "--- Setting up indexing" ;
 RDF_OBJ_FT_RULE_ADD (null, null, 'All');
 VT_INC_INDEX_DB_DBA_RDF_OBJ ();
-echoln --- Populating lookup table
+echoln "--- Populating lookup table" ;
 -- Run the following procedure using the Virtuoso isql program to populate label lookup tables periodically and activate the Label text box of the Entity Label Lookup tab:
 urilbl_ac_init_db();
-echoln --- Ranking IRIs
+echoln "--- Ranking IRIs" ;
 -- Run the following procedure using the Virtuoso isql program to calculate the IRI ranks. Note this should be run periodically as the data grows to re-rank the IRIs.
 s_rank();
 checkpoint;

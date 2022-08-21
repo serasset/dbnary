3d987f32c9dba121ea29ef546ad43e069b95ba80
Gilles Sérasset <gilles.serasset@imag.fr>
2022-08-21 17:36:23 +0200

    fix(chinese): adapting example pattern to cope with Chinese, while maintaining correct behaviour in other languages

--- dbnary-extractor/src/main/java/org/getalp/dbnary/wiki/WikiPatterns.java
+++ dbnary-extractor/src/main/java/org/getalp/dbnary/wiki/WikiPatterns.java
@@ -15,7 +15,7 @@ public class WikiPatterns {
   public final static String multipleBulletListPatternString = "(\\*+)\\s*(.*)";
 
   // public final static String examplePatternString = "^#{1,2}\\*\\s*(.*)$";
-  public final static String examplePatternString = "#{1,2}[\\*:]+(.*)";
+  public final static String examplePatternString = "^#{1,2}[\\*:]+\\s*(.*)$";
 
   public final static String catOrInterwikiLink = "^\\s*\\[\\[([^\\:\\]]*)\\:([^\\]]*)\\]\\]\\s*$";
   public final static Pattern categoryOrInterwikiLinkPattern;

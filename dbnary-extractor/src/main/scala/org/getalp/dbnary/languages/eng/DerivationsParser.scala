f0dddd9d60641f9e2e79a9aa7ead9314edee827d
Gilles Sérasset <gilles.serasset@imag.fr>
2023-02-22 18:23:20 +0100

    fix(english) updated derivations parser

--- dbnary-extractor/src/main/scala/org/getalp/dbnary/languages/eng/DerivationsParser.scala
+++ dbnary-extractor/src/main/scala/org/getalp/dbnary/languages/eng/DerivationsParser.scala
@@ -2,11 +2,11 @@ package org.getalp.dbnary.languages.eng
 
 import com.typesafe.scalalogging.Logger
 import org.apache.commons.lang3.StringUtils
-import org.getalp.dbnary.wiki.{WikiCharSequence, WikiRegexParsers, WikiText}
+import org.getalp.dbnary.wiki.{WikiCharSequence, WikiPattern, WikiRegexParsers, WikiText}
 
-import java.util.Map.Entry
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
+import scala.util.matching.Regex
 
 class DerivationsParser(page: String) extends WikiRegexParsers {
 
@@ -36,7 +36,7 @@ class DerivationsParser(page: String) extends WikiRegexParsers {
     if (StringUtils.isNumeric(k)) {
       val valContent = v
       if (valContent.wikiTokens.isEmpty) { // the value is a string
-        List(Derivation(valContent.toString, null))
+        List(Derivation(valContent.toString.trim, null))
       }
       else {
         val valueParser = new DerivationsParser(page)
@@ -49,28 +49,71 @@ class DerivationsParser(page: String) extends WikiRegexParsers {
     }
   }
 
-  protected def derivationValue: Parser[List[Derivation]] = repsep(
-    derivationsAsLink | derivationLinkAsTemplate | derivationAsPlainText,
-    "or|\\p{Punct}*".r
-  ) ^^ (_.flatten)
+  protected def derivationNotes: Parser[String] =
+    "\\([^)]*\\)".r ^^ (s => {
+      logger.debug("Derivations: ignoring notes `{}` in {}", s, pagename)
+      s
+    })
 
-  protected def derivationAsPlainText: Parser[List[Derivation]] = wikiCharSequenceMatching(".+".r) ^^ (
+  protected def derivationValue: Parser[List[Derivation]] = derivationValueSequence | junkValue
+
+  protected def junkValue: Parser[List[Derivation]] = wikiCharSequenceMatching(".*".r) ^^ (s => {
+    logger.debug("Ignoring derivation value (Junk Value): `{}` in {}", s.getSourceContent(s), pagename )
+    List.empty
+  })
+
+  protected def derivationValueSequence: Parser[List[Derivation]] = (
+    repsep(
+      (derivationsAsLink | derivationLinkAsTemplate | derivationAsPlainText) <~ opt(derivationNotes),
+      "or|\\p{Punct}*".r
+    ) <~ "\\p{Punct}*".r) ^^ (_.flatten)
+
+  protected def derivationAsPlainText: Parser[List[Derivation]] = wikiCharSequenceMatching(("[^" + WikiPattern.RESERVED + "(]+").r) ^^ (
     s => {
       logger.debug("Derivation Value (Plain Text): {} || {}", s.getSourceContent, pagename)
       List(Derivation(s.getSourceContent, null))
     })
 
-  protected def derivationLinkAsTemplate: Parser[List[Derivation]] = linkTemplate | junkTemplate
+  protected def derivationLinkAsTemplate: Parser[List[Derivation]] =
+    linkTemplate | vernTemplate | wTemplate | junkTemplate
 
   protected def linkTemplate: Parser[List[Derivation]] = template("[lL](ink)?".r) ^^ (
     t => {
-      logger.debug("Derivation Value (link Template): {} || {}", t.toString, pagename)
       val args = t.cloneParsedArgs().asScala
       args.remove("1") // removing language declaration
       args.remove("2") // removing target value as it will be taken directly from the template
       if (args.contains("4")) args.addOne("t" -> args("4"))
       args.remove("4")
       List(Derivation(t.getParsedArg("2"), mapAsString(args)))
+    })
+
+  protected def vernTemplate: Parser[List[Derivation]] = template("vern(acular)?".r) ^^ (
+    t => {
+      val args = t.cloneParsedArgs().asScala
+      if (args.contains("2")) args.addOne("plural" -> args("2"))
+      args.remove("lang")
+      args.remove("novern")
+      args.remove("nopedia")
+      if (args.contains("pl")) args.addOne("plural" -> (args("1") + args("pl")))
+      args.remove("1") // removing target value as it will be taken directly from the template
+      List(Derivation(t.getParsedArg("1"), mapAsString(args)))
+    })
+
+  protected def wTemplate: Parser[List[Derivation]] = template("[Ww]".r) ^^ (
+    t => {
+      val args = t.cloneParsedArgs().asScala
+      if (!args.contains("1"))
+        List.empty
+      else {
+        var target = args.getOrElse("1", "")
+        if (args.contains("2")) {
+          args.addOne("wp" -> target)
+          target = args("2")
+          args.remove("2")
+        }
+        args.remove("1") // removing target value as it will be taken directly from the template
+        List(Derivation(target, mapAsString(args)))
+      }
     })
 
   protected def mapAsString(m: mutable.Map[String, String]): String = {
@@ -86,24 +129,46 @@ class DerivationsParser(page: String) extends WikiRegexParsers {
       List.empty
     })
 
+  // TODO: check if links have prefixes (e.g. [[w:XXX]] points to wikipedia)
+  protected val ignoredNameSpaces: Regex =
+    (for (n <- List("CATEGORY", "IMAGE", "THESAURUS"); s <- List(":" + n + ":", n + ":")) yield s).mkString("|").r
+  protected val honoredNameSpaces: Regex = "W:|:W:".r
+
+
   protected def derivationsAsLink: Parser[List[Derivation]] = internalLink() ^^ (
     l => {
-      if (l.getTargetText startsWith "Category") {
-        // Ignore trailing Category links
+      val target = l.getTargetText.toUpperCase()
+
+      if ((ignoredNameSpaces findPrefixOf target).isDefined) {
         List.empty
-      } else if (l.getTargetText == l.getLinkText)
-        List(Derivation(l.getTargetText, null))
-      else
-        List(Derivation(l.getTargetText, "form=" + l.getLinkText))
+      } else {
+        val nameSpace: Option[String] = honoredNameSpaces findPrefixOf target
+        val targetText = l.getTargetText.substring(nameSpace.getOrElse("").length)
+        if (targetText == l.getLinkText)
+          List(Derivation(targetText, null))
+        else
+          List(Derivation(targetText, "form=" + l.getLinkText))
+      }
     })
 
-  protected def junk: Parser[List[Derivation]] = ".".r ^^ (s => {
+  protected def derivationAsListItem: Parser[List[Derivation]] =
+    openClose(listItem(), derivationValue) ^^ {
+      case (_, content) => content
+    }
+
+  protected def junk: Parser[List[Derivation]] = knownJunk | unknownJunk
+
+  protected def knownJunk: Parser[List[Derivation]] = "----".r ^^ (s => {
+    List.empty
+  })
+
+  protected def unknownJunk: Parser[List[Derivation]] = ".".r ^^ (s => {
     logger.debug("Derivation (Junk): {} || {}", s, pagename)
     List.empty
   })
 
   protected def derivationSection: Parser[List[Derivation]] =
-    rep(derivationsAsTemplate | derivationsAsLink | junk) ^^ (l => l.flatten)
+    rep(derivationsAsTemplate | derivationsAsLink | derivationAsListItem | junk) ^^ (l => l.flatten)
 
 
   protected def parseDerivations(input: WikiCharSequence, entry: String): List[Derivation] = {
@@ -117,13 +182,15 @@ class DerivationsParser(page: String) extends WikiRegexParsers {
     }
   }
 
+  // TODO: handle special formatting
+  //  e.g. [[alphanumeric]][[alphanumerical|(-al)]] <small>{{q|by clipping}}</small>
   protected def parseDerivationValues(input: WikiCharSequence, entry: String): List[Derivation] = {
     currentEntry = entry
     source = input
     parseAll(derivationValue, input) match {
       case Success(result, _) => result
       case failure: NoSuccess => {
-        logger.debug("Could not parse derivation values for \"" + entry + "\": " + failure.msg)
+        logger.debug("Could not parse derivation values {} for \"{}\": {}", source.getSourceContent(source), entry, failure.msg)
         Nil
       }
     }
--- dbnary-wikitext/src/main/scala/org/getalp/dbnary/wiki/WikiRegexParsers.scala
+++ dbnary-wikitext/src/main/scala/org/getalp/dbnary/wiki/WikiRegexParsers.scala
@@ -46,6 +46,16 @@ trait WikiRegexParsers extends RegexParsers {
         Failure(message + " expected but end of source found", in.drop(start - offset))
     }
 
+  def decorated[T <: WikiText#Token](p: => Parser[T]): Parser[(Int, T)] = Parser { in =>
+    p(in) match {
+      case Success(t, in1) => {
+        val source = in.source.asInstanceOf[WikiCharSequence]
+        val start = handleWhiteSpace(source, in.offset)
+        Success((source.codePointAt(start), t), in1)
+      }
+      case ns: NoSuccess => ns
+    }
+  }
 
   /**
    * A parser that matches a template whose name matches namePattern
@@ -58,64 +68,21 @@ trait WikiRegexParsers extends RegexParsers {
       c => WikiCharSequence.TEMPLATES_RANGE.contains(c),
       t => namePattern matches t.asInstanceOf[WikiText#Template].getName)
       .^^(t => t.asInstanceOf[WikiText#Template])
-  //def template(namePattern: Regex): Parser[WikiText#Template] = (in: Input) => {
-  //    val source = in.source.asInstanceOf[WikiCharSequence]
-  //    val offset = in.offset
-  //    val start = handleWhiteSpace(source, offset)
-  //    if (start < source.length()) {
-  //      val c = source.codePointAt(start)
-  //      if (WikiCharSequence.TEMPLATES_RANGE.contains(c)) {
-  //        val tmpl = source.getToken(c).asInstanceOf[WikiText#Template]
-  //        if (namePattern matches tmpl.getName) {
-  //          Success(tmpl, in.drop(start - offset + Character.charCount(c)))
-  //
-  //} else {
-  //          Failure("Template with name matching `" + namePattern + "' expected but Template named" + tmpl.getName + " found", in.drop(start - offset))
-  //
-  // }
-  //}
-  //      else
-  //        Failure("Template with name matching `" + namePattern + "' expected but " + source.getSourceContent(strFromCodePoint(c)) + " found", in.drop(start - offset))
-  //    }
-  //    else
-  //      Failure("Template with name matching `" + namePattern + "' expected but end of source found", in.drop(start - offset))
-  //  }
 
-  /** A parser that matches a template with given name */
+  /** A parser that matches a template whose name is templateName */
   def template(templateName: String): Parser[WikiText#Template] =
     wikiEvent("Template with name `" + templateName + "'",
       c => WikiCharSequence.TEMPLATES_RANGE.contains(c),
       t => templateName == t.asInstanceOf[WikiText#Template].getName)
       .^^(t => t.asInstanceOf[WikiText#Template])
-  //  val source = in.source.asInstanceOf[WikiCharSequence]
-  //  val offset = in.offset
-  //  val start = handleWhiteSpace(source, offset)
-  //  if (start < source.length()) {
-  //    val c = source.codePointAt(start)
-  //    if (WikiCharSequence.TEMPLATES_RANGE.contains(c)) {
-  //      val tmpl = source.getToken(c).asInstanceOf[WikiText#Template]
-  //      if (tmpl.getName == templateName) {
-  //        Success(tmpl, in.drop(start - offset + Character.charCount(c)))
-  //
-  //      } else {
-  //        Failure("Template with name `" + templateName + "' expected but Template named" + tmpl.getName + " found", in.drop(start - offset))
-  //
-  //      }
-  //    }
-  //    else
-  //      Failure("Template with name `" + templateName + "' expected but " + source.getSourceContent(strFromCodePoint(c)) + " found", in.drop(start - offset))
-  //  }
-  //  else
-  //    Failure("Template with name `" + templateName + "' expected but end of source found", in.drop(start - offset))
-  //}
+
+  /*--------- Simple matchers on token types ------------*/
 
   /** A parser that matches any template */
   def template(): Parser[WikiText#Template] = wikiEvent(
     "Template",
     c => WikiCharSequence.TEMPLATES_RANGE.contains(c)
   ) ^^ (t => t.asInstanceOf[WikiText#Template])
-
-  /*--------- Simple matchers on token types ------------*/
 
   /** A parser that matches any link */
   def link(): Parser[WikiText#Link] =
@@ -138,6 +105,29 @@ trait WikiRegexParsers extends RegexParsers {
       c => WikiCharSequence.EXTERNAL_LINKS_RANGE.contains(c)
     ) ^^ (l => l.asInstanceOf[WikiText#ExternalLink])
 
+  /** A parser that matches any indented item */
+  def indentedItem(): Parser[WikiText#IndentedItem] =
+    wikiEvent(
+      "Indented Item",
+      c => WikiCharSequence.LISTS_RANGE.contains(c)
+    ) ^^ (l => l.asInstanceOf[WikiText#IndentedItem])
+
+  /** A parser that matches any list item */
+  def listItem(): Parser[WikiText#ListItem] =
+    wikiEvent(
+      "List Item",
+      c => WikiCharSequence.LISTS_RANGE.contains(c),
+      t => t.isInstanceOf[WikiText#ListItem]
+    ) ^^ (l => l.asInstanceOf[WikiText#ListItem])
+
+  /** A parser that matches any numbered list item */
+  def numberedListItem(): Parser[WikiText#NumberedListItem] =
+    wikiEvent(
+      "External link",
+      c => WikiCharSequence.LISTS_RANGE.contains(c),
+      t => t.isInstanceOf[WikiText#NumberedListItem]
+    ) ^^ (l => l.asInstanceOf[WikiText#NumberedListItem])
+
   /*
   Redefine the handling of patterns so that they return the match instead of the matched string
  */
@@ -156,6 +146,23 @@ trait WikiRegexParsers extends RegexParsers {
   }
 
   def strFromCodePoint(c: Int): String = new String(Character.toChars(c))
+
+  def openClose[T](tokenParser: Parser[WikiText#Token], contentParser: Parser[T]): Parser[(WikiText#Token, T)] = Parser { in =>
+    var tokenChar = 0
+    (openToken ~> decorated(tokenParser)) (in) match {
+      case Success((c, token), in1) =>
+        tokenChar = c
+        (contentParser <~ wikiEvent("Specific token char", _ == tokenChar) ~ closeToken) (in1) match {
+          case Success(content, rest) => Success((token, content), rest)
+          case ns: NoSuccess => ns
+        }
+      case ns: NoSuccess => ns
+    }
+  }
+
+  def openToken: Parser[String] = literal("〔")
+
+  def closeToken: Parser[String] = "〕"
 
   /** A parser that matches a literal string */
   override implicit def literal(s: String): Parser[String]

7aac67a2937c3f75db95f4938505b6f146e6e67c
Gilles Sérasset <gilles.serasset@imag.fr>
2023-02-22 18:24:13 +0100

    fix(english) workaround a bug in bliki eval of English str left template

--- dbnary-extractor/src/main/java/org/getalp/dbnary/languages/eng/EnglishWikiModel.java
+++ dbnary-extractor/src/main/java/org/getalp/dbnary/languages/eng/EnglishWikiModel.java
@@ -1,5 +1,8 @@
 package org.getalp.dbnary.languages.eng;
 
+import info.bliki.wiki.filter.ParsedPageName;
+import info.bliki.wiki.model.WikiModelContentException;
+import info.bliki.wiki.namespaces.INamespace.NamespaceCode;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Map;
@@ -19,6 +22,14 @@ public class EnglishWikiModel extends DbnaryWikiModel {
     if (!templateName.startsWith("tracking")) {
       if (templateName.equals("check deprecated lang param usage")) {
         writer.append(parameterMap.get("1"));
+      } else if ("str left".equals(templateName)) {
+        try {
+          int l = Integer.parseInt(parameterMap.get("2"));
+          String val = parameterMap.getOrDefault("1", "");
+          writer.append(val.substring(0, Math.min(l, val.length())));
+        } catch (NumberFormatException e) {
+
+        }
       } else {
         super.substituteTemplateCall(templateName, parameterMap, writer);
       }

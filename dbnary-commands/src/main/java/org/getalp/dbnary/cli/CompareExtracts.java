8aea28062998a434e8649c96f29acc57346c0668
Gilles Sérasset <gilles.serasset@imag.fr>
2023-05-08 18:31:04 +0200

    fix(commands): compare command now check if file exists before fetching enh(commands): compare command now creates a temp TDB if turtle file is too big enh(commands): compare command now transfer verbose status to rdfdiff command

--- dbnary-commands/src/main/java/org/getalp/dbnary/cli/CompareExtracts.java
+++ dbnary-commands/src/main/java/org/getalp/dbnary/cli/CompareExtracts.java
@@ -7,9 +7,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Callable;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.http.HttpEntity;
@@ -28,12 +27,14 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.ParentCommand;
 import picocli.CommandLine.Spec;
+import tdb2.tdbloader;
 
 @Command(name = "compare", mixinStandardHelpOptions = true,
     header = "fetch and compare extracts from different dates.",
     description = "Fetches 2 different dumps and compute their differences.")
 public class CompareExtracts implements Callable<Integer> {
 
+  private static final long IN_MEMORY_FILESIZE_LIMIT = 100000000;
   // CommandLine specification
   @Spec
   protected CommandSpec spec;
@@ -75,32 +76,6 @@ public class CompareExtracts implements Callable<Integer> {
   private Path toDir;
   private Path diffDir;
 
-  private static class LanguageConfiguration {
-    String lang;
-    Map<ExtractionFeature, Path> extracts = new HashMap<>();
-    Map<ExtractionFeature, Path> exolexExtracts = new HashMap<>();
-
-    private LanguageConfiguration(String lang) {
-      this.lang = lang;
-    }
-
-    private Path geEndolextExtract(ExtractionFeature f) {
-      return extracts.get(f);
-    }
-
-    private Path geExolextExtract(ExtractionFeature f) {
-      return exolexExtracts.get(f);
-    }
-
-    private Path setEndolexExtract(ExtractionFeature f, Path p) {
-      return extracts.put(f, p);
-    }
-
-    public Path setExolexExtract(ExtractionFeature f, Path p) {
-      return exolexExtracts.put(f, p);
-    }
-  }
-
   public Integer call() {
     return fetchAndCompareDumps();
   }
@@ -110,6 +85,7 @@ public class CompareExtracts implements Callable<Integer> {
       fromDir = Files.createDirectories(output.resolve("from"));
       toDir = Files.createDirectories(output.resolve("to"));
       fetchRequestedDumps();
+      prepareTDBForBigDumps();
       diffDir = Files.createDirectories(output.resolve("diffs"));
       compareExtracts();
       return 0;
@@ -119,8 +95,63 @@ public class CompareExtracts implements Callable<Integer> {
     }
   }
 
+  private void prepareTDBForBigDumps() {
+    Arrays.stream(languages).distinct().sequential().forEach(this::prepareTDBForBigDumps);
+  }
+
+  private void prepareTDBForBigDumps(String lang) {
+    features.getEndolexFeatures()
+        .forEach(ft -> createTDBIfTooBig(lang, ft, fromDate, false, fromDir));
+    features.getExolexFeatures()
+        .forEach(ft -> createTDBIfTooBig(lang, ft, fromDate, true, fromDir));
+    features.getEndolexFeatures().forEach(ft -> createTDBIfTooBig(lang, ft, toDate, false, toDir));
+    features.getExolexFeatures().forEach(ft -> createTDBIfTooBig(lang, ft, toDate, true, toDir));
+  }
+
+  private void createTDBIfTooBig(String lang, ExtractionFeature ft, String date, boolean isExolex,
+      Path folder) {
+    try {
+      Path turtleFile = folder
+          .resolve(ExtractionPreferences.outputFilename(ft, lang, date, "TURTLE", false, isExolex));
+      Path tdbPath = turtleFile.resolveSibling(turtleFile.getFileName().toString() + ".tdb");
+      if (Files.exists(tdbPath)) {
+        if (Files.isDirectory(tdbPath)) {
+          spec.commandLine().getErr()
+              .format("TDB directory %s already exists:  ==> I will use it %n"
+                  + "      (if you want it to be recreated relaunch command after removing the folder).%n",
+                  tdbPath);
+        } else {
+          spec.commandLine().getErr().format("A file named %s already exists:  ==> Stopping.%n",
+              tdbPath);
+          System.exit(-1);
+        }
+      } else {
+        long turtleFileSize = Files.size(turtleFile);
+        if (turtleFileSize > IN_MEMORY_FILESIZE_LIMIT) {
+          if (parent.isVerbose()) {
+            spec.commandLine().getErr().format("Creating TDB for %s (file size: %d) %n", turtleFile,
+                turtleFileSize);
+          }
+          ArrayList<String> a = new ArrayList<>();
+          // --loc "$tdbfile" "$ttlfile"
+          a.add("--loc");
+          a.add(tdbPath.toString());
+          a.add(turtleFile.toString());
+          String[] args = a.toArray(new String[0]);
+          tdbloader.main(args);
+        }
+      }
+
+    } catch (IOException e) {
+      spec.commandLine().getErr()
+          .println("IOException while preparing TDB: " + e.getLocalizedMessage());
+      System.exit(-2);
+    }
+  }
+
+
   private void compareExtracts() {
-    Arrays.stream(languages).distinct().parallel().forEach(l -> compareExtracts(l));
+    Arrays.stream(languages).distinct().parallel().forEach(this::compareExtracts);
   }
 
   private void compareExtracts(String lang) {
@@ -134,17 +165,42 @@ public class CompareExtracts implements Callable<Integer> {
       boolean isExolex) {
     Path from = fromDir.resolve(
         ExtractionPreferences.outputFilename(ft, lang, fromDate, "TURTLE", false, isExolex));
+    Path fromTDB = fromDir
+        .resolve(ExtractionPreferences.outputFilename(ft, lang, fromDate, "TURTLE", false, isExolex)
+            + ".tdb");
     Path to = toDir
         .resolve(ExtractionPreferences.outputFilename(ft, lang, toDate, "TURTLE", false, isExolex));
-    if (Files.isReadable(from) && Files.isReadable(to)) {
+    Path toTDB = toDir.resolve(
+        ExtractionPreferences.outputFilename(ft, lang, toDate, "TURTLE", false, isExolex) + ".tdb");
+    if (Files.isDirectory(fromTDB)) {
+      from = fromTDB;
+    }
+    if (Files.isDirectory(toTDB)) {
+      to = toTDB;
+    }
+
+    if ((Files.isReadable(from) || Files.isDirectory(from))
+        && (Files.isReadable(to) || Files.isDirectory(to))) {
       try {
         Path lost = diffDir.resolve(lang + "_lost_" + (isExolex ? "exolex_" : "") + ft + ".ttl");
         Path gain = diffDir.resolve(lang + "_gain_" + (isExolex ? "exolex_" : "") + ft + ".ttl");
         spec.commandLine().getErr().format("Comparing %s and %s%n", from, to);
         spec.commandLine().getErr().format("    --> %s%n", lost);
-        RDFDiff.main(new String[] {String.valueOf(from), String.valueOf(to), String.valueOf(lost)});
+        ArrayList<String> a = new ArrayList<>();
+        if (parent.isVerbose())
+          a.add("-v");
+        a.add(String.valueOf(from));
+        a.add(String.valueOf(to));
+        a.add(String.valueOf(lost));
+        RDFDiff.main(a.toArray(new String[0]));
         spec.commandLine().getErr().format("    --> %s%n", gain);
-        RDFDiff.main(new String[] {String.valueOf(to), String.valueOf(from), String.valueOf(gain)});
+        a.clear();
+        if (parent.isVerbose())
+          a.add("-v");
+        a.add(String.valueOf(to));
+        a.add(String.valueOf(from));
+        a.add(String.valueOf(gain));
+        RDFDiff.main(a.toArray(new String[0]));
       } catch (FileNotFoundException e) {
         e.printStackTrace();
       }
@@ -177,9 +233,17 @@ public class CompareExtracts implements Callable<Integer> {
   private Path fetchExtract(CloseableHttpClient client, String lang, ExtractionFeature f,
       String date, boolean isExolex, Path folder) {
     try {
+      Path outputExtract = folder
+          .resolve(ExtractionPreferences.outputFilename(f, lang, date, "TURTLE", false, isExolex));
+      if (Files.isReadable(outputExtract)) {
+        spec.commandLine().getErr().format("File %s already exists, I won't fecth it again.",
+            outputExtract);
+        return outputExtract;
+      }
+
       URL url = new URL(server);
       if (!url.getProtocol().equals("http")) { // URL protocol is not https
-        System.err.format("Unsupported protocol: %s", url.getProtocol());
+        spec.commandLine().getErr().format("Unsupported protocol: %s", url.getProtocol());
         return null;
       }
 
@@ -187,10 +251,6 @@ public class CompareExtracts implements Callable<Integer> {
           RDFLanguages.TURTLE.getName(), true, isExolex);
       String fullExtractURL =
           server + "ontolex/" + ((null == date) ? "latest" : lang) + "/" + extractFilename;
-      Path outputExtract = folder
-          .resolve(ExtractionPreferences.outputFilename(f, lang, date, "TURTLE", false, isExolex));
-
-
       if (parent.isVerbose()) {
         spec.commandLine().getErr().format("Fetching %s%n", fullExtractURL);
       }
@@ -206,14 +266,15 @@ public class CompareExtracts implements Callable<Integer> {
 
         if (entity != null) {
           if (parent.isVerbose()) {
-            System.err.println("Retrieving and uncompressing data from : " + fullExtractURL);
+            spec.commandLine().getErr()
+                .println("Retrieving and uncompressing data from : " + fullExtractURL);
           }
           try (
               BZip2CompressorInputStream bzIn = new BZip2CompressorInputStream(entity.getContent());
               OutputStream out = Files.newOutputStream(outputExtract)) {
             long s = System.currentTimeMillis();
             final byte[] buffer = new byte[2048];
-            int read = 0, total = 0;
+            int read, total = 0;
             while (-1 != (read = bzIn.read(buffer))) {
               if (parent.isVerbose()) {
                 total += read;
@@ -223,8 +284,8 @@ public class CompareExtracts implements Callable<Integer> {
               }
               out.write(buffer, 0, read);
             }
-            System.err.println("Fetched and uncompressed to " + outputExtract + "["
-                + (System.currentTimeMillis() - s) + " ms]");
+            spec.commandLine().getErr().println("Fetched and uncompressed to " + outputExtract
+                + " [" + (System.currentTimeMillis() - s) + " ms]");
           }
         }
       } catch (IOException e) {
@@ -235,7 +296,7 @@ public class CompareExtracts implements Callable<Integer> {
 
       return outputExtract;
     } catch (MalformedURLException e) {
-      System.err.format("Malformed dump server URL: %s", server);
+      spec.commandLine().getErr().format("Malformed dump server URL: %s", server);
       return null;
     }
   }

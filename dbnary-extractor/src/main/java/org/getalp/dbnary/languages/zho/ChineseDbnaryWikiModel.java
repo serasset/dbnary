357c1dd2e5be6bfeef04abba3dfe73a308d29946
Gilles Serasset <gilles.serasset@univ-grenoble-alpes.fr>
2024-06-12 09:55:22 +0200

    fix(zho): the Chinese wiktionary borrows scripts from Englis editions that contains Lua 5.1 specific deprecated code that is not handled by DBnary (5.2) Lua engine

--- dbnary-extractor/src/main/java/org/getalp/dbnary/languages/zho/ChineseDbnaryWikiModel.java
+++ dbnary-extractor/src/main/java/org/getalp/dbnary/languages/zho/ChineseDbnaryWikiModel.java
@@ -7,8 +7,9 @@ import java.util.Locale;
 import java.util.Map;
 import org.getalp.dbnary.api.WiktionaryPageSource;
 import org.getalp.dbnary.bliki.DbnaryWikiModel;
+import org.getalp.dbnary.languages.commons.EnglishLikeModulesPatcherWikiModel;
 
-public class ChineseDbnaryWikiModel extends DbnaryWikiModel {
+public class ChineseDbnaryWikiModel extends EnglishLikeModulesPatcherWikiModel {
 
   public ChineseDbnaryWikiModel(WiktionaryPageSource wi, Locale locale, String imageBaseURL,
       String linkBaseURL) {
@@ -18,6 +19,7 @@ public class ChineseDbnaryWikiModel extends DbnaryWikiModel {
   @Override
   public String getRawWikiContent(ParsedPageName parsedPagename, Map<String, String> map)
       throws WikiModelContentException {
+    // Chinese edition systematically capitalize modules and templates names
     ParsedPageName originalPagename = parsedPagename;
     if ((parsedPagename.namespace.isType(NamespaceCode.TEMPLATE_NAMESPACE_KEY)
         || parsedPagename.namespace.isType(NamespaceCode.MODULE_NAMESPACE_KEY))
@@ -31,6 +33,7 @@ public class ChineseDbnaryWikiModel extends DbnaryWikiModel {
     String wikiContent = super.getRawWikiContent(parsedPagename, map);
     if (null == wikiContent) {
       // Chinese templates are supposed to begin with an uppercase char
+      // but if the uppercase variant does not exist, we try the original name
       wikiContent = super.getRawWikiContent(originalPagename, map);
     }
     return wikiContent;

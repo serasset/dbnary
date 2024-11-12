45d62228b50835b02e621c7800c0fd3a230cd843
Gilles Sérasset <gilles.serasset@imag.fr>
2024-11-12 10:01:46 +0100

    fix(cicd): avoid failure in latest diff to pollute the script return status

--- .gitlab-ci/compute_differences.sh
+++ .gitlab-ci/compute_differences.sh
@@ -12,5 +12,7 @@ do
   # compute differences
   echo "${CI_PROJECT_DIR}/kaiko/extractor/compute_diffs.sh -m $model -f target/evaluation/$PREVIOUS_VERSION/extracts/ontolex/latest/ -t target/evaluation/$NEXT_VERSION/extracts/ontolex/latest/ -d $DIFFS -x $NEXT_VERSION_BIN/rdfdiff $LANGS"
   # shellcheck disable=SC2086
-  "${CI_PROJECT_DIR}/kaiko/extractor/compute_diffs.sh" -m $model -f "target/evaluation/$PREVIOUS_VERSION/extracts/ontolex/latest/" -t "target/evaluation/$NEXT_VERSION/extracts/ontolex/latest/" -d "$DIFFS" -x "$NEXT_VERSION_BIN/rdfdiff" $LANGS
+  "${CI_PROJECT_DIR}/kaiko/extractor/compute_diffs.sh" -m $model -f "target/evaluation/$PREVIOUS_VERSION/extracts/ontolex/latest/" -t "target/evaluation/$NEXT_VERSION/extracts/ontolex/latest/" -d "$DIFFS" -x "$NEXT_VERSION_BIN/rdfdiff" $LANGS || echo "Failed to compute differences for $model"
 done
+
+echo "Differences computed and stored in $DIFFS"
